3a23169e306e97cc2a9f41ffbdb269cb5704d292
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2015-04-17 15:14:18 +0200

    Now deserializing names according to specific type in serialization.

--- commons/src/main/java/cc/kave/commons/utils/json/GsonNameDeserializer.java
+++ commons/src/main/java/cc/kave/commons/utils/json/GsonNameDeserializer.java
@@ -3,6 +3,8 @@ package cc.kave.commons.utils.json;
 import java.lang.reflect.Type;
 
 import cc.kave.commons.model.names.Name;
+import cc.kave.commons.model.names.csharp.CsAliasName;
+import cc.kave.commons.model.names.csharp.CsAssemblyName;
 import cc.kave.commons.model.names.csharp.CsName;
 
 import com.google.gson.JsonDeserializationContext;
@@ -13,10 +15,21 @@ import com.google.gson.JsonParseException;
 public class GsonNameDeserializer implements JsonDeserializer<Name> {
 
 	@Override
-	public Name deserialize(JsonElement json, Type typeOfT,
-			JsonDeserializationContext context) throws JsonParseException {
-		final String identifier = json.getAsString().split(":")[1];
-        return CsName.newName(identifier);
+	public Name deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+			throws JsonParseException {
+		String[] nameInfo = json.getAsString().split(":");
+		String discriminator = nameInfo[0];
+		final String identifier = nameInfo[1];
+		switch (discriminator) {
+		case "CSharp.AliasName":
+			return CsAliasName.newAliasName(identifier);
+		case "CSharp.AssemblyName":
+			return CsAssemblyName.newAssemblyName(identifier);
+		case "CSharp.Name":
+			return CsName.newName(identifier);
+		default:
+			throw new IllegalArgumentException("Not a valid serialized name: '" + json + "'");
+		}
 	}
 
 }
--- commons/src/test/java/commons/CSharpNameDeserializationTest.java
+++ commons/src/test/java/commons/CSharpNameDeserializationTest.java
@@ -5,7 +5,11 @@ import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
 
+import cc.kave.commons.model.names.AliasName;
+import cc.kave.commons.model.names.BundleName;
 import cc.kave.commons.model.names.Name;
+import cc.kave.commons.model.names.csharp.CsAliasName;
+import cc.kave.commons.model.names.csharp.CsAssemblyName;
 import cc.kave.commons.model.names.csharp.CsName;
 import cc.kave.commons.utils.json.GsonNameDeserializer;
 
@@ -15,24 +19,40 @@ import com.google.gson.GsonBuilder;
 public class CSharpNameDeserializationTest {
 	@Test
 	public void DeserializesToCsName() {
-		CsName name = parseJson("\"CSharp.Name:Identifier\"", CsName.class);
-
-		assertThat(name, sameInstance(CsName.newName("Identifier")));
+		assertDeserialize("\"CSharp.Name:Identifier\"", CsName.newName("Identifier"), Name.class);
 	}
-	
+
 	@Test
-	public void DeserializesToName() {
-		Name name = parseJson("\"CSharp.Name:Identifier\"", Name.class);
-		
-		assertThat(name, sameInstance(CsName.newName("Identifier")));
+	public void DeserializesToAliasName() {
+		assertDeserialize("\"CSharp.AliasName:alias\"", CsAliasName.newAliasName("alias"), AliasName.class);
+	}
+
+	@Test
+	public void DeserializesToAssemblyName() {
+		assertDeserialize("\"CSharp.AssemblyName:alias\"", CsAssemblyName.newAssemblyName("alias"), BundleName.class);
+	}
+
+	private void assertDeserialize(String json, Name expectedInstance, Class<? extends Name> mostSpecificInterface) {
+		assertDeserialize(json, Name.class, expectedInstance);
+		assertDeserialize(json, expectedInstance.getClass(), expectedInstance);
+		assertDeserialize(json, mostSpecificInterface, expectedInstance);
+	}
+
+	private void assertDeserialize(String json, Class<? extends Name> requestedType, Name expectedInstance) {
+		Name name = parseJson(json, requestedType);
+		assertThat(name, sameInstance(expectedInstance));
 	}
 
 	private <T> T parseJson(String json, Class<T> targetType) {
 		GsonBuilder gsonBuilder = new GsonBuilder();
-		gsonBuilder.registerTypeAdapter(CsName.class, new GsonNameDeserializer());
+		gsonBuilder.registerTypeAdapter(AliasName.class, new GsonNameDeserializer());
+		gsonBuilder.registerTypeAdapter(BundleName.class, new GsonNameDeserializer());
 		gsonBuilder.registerTypeAdapter(Name.class, new GsonNameDeserializer());
+		gsonBuilder.registerTypeAdapter(CsAliasName.class, new GsonNameDeserializer());
+		gsonBuilder.registerTypeAdapter(CsAssemblyName.class, new GsonNameDeserializer());
+		gsonBuilder.registerTypeAdapter(CsName.class, new GsonNameDeserializer());
 		Gson gson = gsonBuilder.create();
-		
+
 		return gson.fromJson(json, targetType);
 	}
 }
