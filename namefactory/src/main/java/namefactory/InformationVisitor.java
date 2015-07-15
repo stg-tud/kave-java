d9c2398fe1f24e7c8aa0190c435622f7fe8013b0
Waldemar Graf <Wgraf91@gmail.com>
2015-07-15 10:51:18 +0200

    Added NodeFactory with some tests

--- namefactory/src/main/java/namefactory/InformationVisitor.java
+++ namefactory/src/main/java/namefactory/InformationVisitor.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2015 Waldemar Graf
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package namefactory;
 
 import java.util.ArrayList;
@@ -8,34 +24,44 @@ import org.eclipse.jdt.core.dom.FieldDeclaration;
 import org.eclipse.jdt.core.dom.ImportDeclaration;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
 import org.eclipse.jdt.core.dom.PackageDeclaration;
+import org.eclipse.jdt.core.dom.TypeDeclaration;
 
 public class InformationVisitor extends ASTVisitor {
 	private List<MethodDeclaration> methods = new ArrayList<MethodDeclaration>();
 	private List<FieldDeclaration> fields = new ArrayList<FieldDeclaration>();
-	private List<PackageDeclaration> packages = new ArrayList<PackageDeclaration>();
+	private PackageDeclaration packages;
 	private List<ImportDeclaration> imports = new ArrayList<ImportDeclaration>();
 
 	@Override
 	public boolean visit(FieldDeclaration node) {
 		fields.add(node);
+		NodeFactory.getNodeName(node);
 		return super.visit(node);
 	}
 
 	@Override
 	public boolean visit(MethodDeclaration node) {
 		methods.add(node);
+		NodeFactory.getNodeName(node);
 		return super.visit(node);
 	}
 
 	@Override
 	public boolean visit(PackageDeclaration node) {
-		packages.add(node);
+		packages = node;
+		return super.visit(node);
+	}
+	
+	@Override
+	public boolean visit(TypeDeclaration node) {
+		NodeFactory.getNodeName(node);
 		return super.visit(node);
 	}
 
 	@Override
 	public boolean visit(ImportDeclaration node) {
 		imports.add(node);
+		NodeFactory.getNodeName(node);
 		return super.visit(node);
 	}
 
@@ -47,7 +73,7 @@ public class InformationVisitor extends ASTVisitor {
 		return fields;
 	}
 
-	public List<PackageDeclaration> getPackages() {
+	public PackageDeclaration getPackages() {
 		return packages;
 	}
 
---
+++ namefactory/src/main/java/namefactory/NodeFactory.java
@@ -0,0 +1,208 @@
+/**
+ * Copyright 2015 Waldemar Graf
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package namefactory;
+
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IMemberValuePair;
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.FieldDeclaration;
+import org.eclipse.jdt.core.dom.IAnnotationBinding;
+import org.eclipse.jdt.core.dom.IBinding;
+import org.eclipse.jdt.core.dom.IMethodBinding;
+import org.eclipse.jdt.core.dom.IPackageBinding;
+import org.eclipse.jdt.core.dom.ITypeBinding;
+import org.eclipse.jdt.core.dom.IVariableBinding;
+import org.eclipse.jdt.core.dom.ImportDeclaration;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.Modifier;
+import org.eclipse.jdt.core.dom.PackageDeclaration;
+import org.eclipse.jdt.core.dom.SingleVariableDeclaration;
+import org.eclipse.jdt.core.dom.TypeDeclaration;
+import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
+
+import cc.kave.commons.model.names.Name;
+import cc.kave.commons.model.names.csharp.CsAssemblyName;
+import cc.kave.commons.model.names.csharp.CsFieldName;
+import cc.kave.commons.model.names.csharp.CsMethodName;
+import cc.kave.commons.model.names.csharp.CsNamespaceName;
+import cc.kave.commons.model.names.csharp.CsParameterName;
+import cc.kave.commons.model.names.csharp.CsTypeName;
+
+public class NodeFactory {
+
+	public static Name getNodeName(ASTNode node) {
+		StringBuilder sb = new StringBuilder();
+
+		switch (node.getNodeType()) {
+		case ASTNode.METHOD_DECLARATION:
+			MethodDeclaration methodNode = (MethodDeclaration) node;
+			IMethodBinding method = methodNode.resolveBinding();
+			BindingFactory.modifierHelper(sb, method);
+			IJavaElement javaElement = method.getJavaElement();
+
+			sb.append("[").append(
+					BindingFactory.getBindingName(method.getReturnType()));
+			sb.append("] ");
+			sb.append("[");
+			sb.append(BindingFactory.getBindingName(method.getDeclaringClass()));
+			sb.append("].");
+			sb.append(method.getName());
+			sb.append("(");
+
+			ITypeBinding[] parameterTypes = method.getParameterTypes();
+			for (int i = 0; i < parameterTypes.length; i++) {
+				StringBuilder param = new StringBuilder();
+				param.append("[");
+				param.append(BindingFactory.getBindingName(parameterTypes[i]));
+				param.append("] ");
+				param.append(((SingleVariableDeclaration) methodNode
+						.parameters().get(i)).getName().getIdentifier());
+
+				CsParameterName.newParameterName(param.toString());
+				sb.append(param.toString());
+
+				if (i < parameterTypes.length - 1) {
+					sb.append(", ");
+				}
+			}
+			sb.append(")");
+
+			return CsMethodName.newMethodName(sb.toString());
+
+		case ASTNode.FIELD_DECLARATION:
+			FieldDeclaration fieldNode = (FieldDeclaration) node;
+
+			Object o = fieldNode.fragments().get(0);
+			if (o instanceof VariableDeclarationFragment) {
+				BindingFactory.modifierHelper(sb,
+						((VariableDeclarationFragment) o).resolveBinding());
+				sb.append("[");
+				sb.append(BindingFactory
+						.getBindingName(((VariableDeclarationFragment) o)
+								.resolveBinding().getType()));
+				sb.append("] ");
+				sb.append("[ ].");
+				sb.append(((VariableDeclarationFragment) o).getName()
+						.getIdentifier());
+			}
+			return CsFieldName.newFieldName(sb.toString());
+
+		case ASTNode.IMPORT_DECLARATION:
+			ImportDeclaration importNode = (ImportDeclaration) node;
+			return CsAssemblyName.newAssemblyName(importNode.getName()
+					.getFullyQualifiedName());
+
+		case ASTNode.TYPE_DECLARATION:
+			TypeDeclaration typeNode = (TypeDeclaration) node;
+			return CsTypeName.newTypeName(BindingFactory
+					.getBindingName(typeNode.resolveBinding()));
+
+		case ASTNode.PACKAGE_DECLARATION:
+			PackageDeclaration packageNode = (PackageDeclaration) node;
+			return CsNamespaceName.newNamespaceName(packageNode
+					.resolveBinding().getName());
+		}
+		return null;
+	}
+
+	public static class BindingFactory {
+
+		public static String getBindingName(IBinding binding) {
+			StringBuilder sb = new StringBuilder();
+
+			switch (binding.getKind()) {
+
+			// IPackageBinding
+			case 1:
+				((IPackageBinding) binding).getName();
+				break;
+			// ITypeBinding
+			case 2:
+				ITypeBinding type = (ITypeBinding) binding;
+
+				if (type.isParameterizedType()) {
+					sb.append(type.getQualifiedName().substring(0,
+							type.getQualifiedName().indexOf("<")));
+					sb.append("`");
+					sb.append(type.getTypeArguments().length);
+					sb.append("[");
+
+					// TODO: generics for later work
+					// for (ITypeBinding t : type.getTypeArguments()) {
+					// sb.append("[T -> ");
+					// sb.append(t.getQualifiedName());
+					// sb.append("],");
+					// }
+					sb.append("GenericsPlaceholder");
+
+					sb.deleteCharAt(sb.toString().length() - 1);
+					sb.append("]");
+				} else {
+					sb.append(type.getQualifiedName());
+				}
+
+				sb.append(", ");
+				String qualifiedName = type.getQualifiedName();
+				if (qualifiedName.contains("."))
+					sb.append(qualifiedName.substring(0,
+							qualifiedName.lastIndexOf(".")));
+				else
+					sb.append(qualifiedName);
+				sb.append(", VersionPlaceholder");
+
+				return sb.toString();
+
+				// IVariableBinding
+			case 3:
+				((IVariableBinding) binding).getType().getQualifiedName();
+				break;
+			// IMethodBinding
+			case 4:
+				((IMethodBinding) binding).getName();
+				break;
+			// IAnnotationBinding
+			case 5:
+				((IAnnotationBinding) binding).getAnnotationType()
+						.getQualifiedName();
+				break;
+			// IMemberValuePair
+			case 6:
+				((IMemberValuePair) binding).getMemberName();
+				break;
+			default:
+				return null;
+			}
+			return null;
+		}
+
+		public static StringBuilder modifierHelper(StringBuilder sb,
+				IBinding binding) {
+			int modifier = binding.getModifiers();
+
+			if (Modifier.isStatic(modifier))
+				sb.append("static ");
+			if (Modifier.isFinal(modifier))
+				sb.append("final ");
+			if (Modifier.isAbstract(modifier))
+				sb.append("abstract ");
+			if (Modifier.isSynchronized(modifier))
+				sb.append("synchronized ");
+
+			return sb;
+		}
+	}
+}
