3f152cc9797045ecf98d8642f9f5c5dc19a3a3b8
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2015-07-27 08:09:56 +0200

    Fixed formatting.

--- histories/src/main/java/cc/kave/histories/model/OUSnapshot.java
+++ histories/src/main/java/cc/kave/histories/model/OUSnapshot.java
@@ -8,56 +8,56 @@ import javax.persistence.*;
 @Table(name = "OUHistories")
 public class OUSnapshot {
 
-	public OUSnapshot() {
-	}
+    public OUSnapshot() {
+    }
 
-	public OUSnapshot(String workPeriod, Date timestamp,
-			String enclosingMethod, String targetType, String objectUsage,
-			boolean isQuery) {
-		this.workPeriod = workPeriod;
-		this.timestamp = timestamp;
-		this.enclosingMethod = enclosingMethod;
-		this.targetType = targetType;
-		this.objectUsage = objectUsage;
-		this.isQuery = isQuery;
-	}
+    public OUSnapshot(String workPeriod, Date timestamp,
+                      String enclosingMethod, String targetType, String objectUsage,
+                      boolean isQuery) {
+        this.workPeriod = workPeriod;
+        this.timestamp = timestamp;
+        this.enclosingMethod = enclosingMethod;
+        this.targetType = targetType;
+        this.objectUsage = objectUsage;
+        this.isQuery = isQuery;
+    }
 
-	@Id
-	@GeneratedValue
-	@Column(name = "Id")
-	private int id;
+    @Id
+    @GeneratedValue
+    @Column(name = "Id")
+    private int id;
 
-	@Column(name = "WorkPeriod")
-	private String workPeriod;
+    @Column(name = "WorkPeriod")
+    private String workPeriod;
 
-	@Column(name = "Timestamp", columnDefinition = "DATETIME")
-	private Date timestamp;
+    @Column(name = "Timestamp", columnDefinition = "DATETIME")
+    private Date timestamp;
 
-	@Column(name = "EnclosingMethod")
-	private String enclosingMethod;
+    @Column(name = "EnclosingMethod")
+    private String enclosingMethod;
 
-	@Column(name = "TargetType")
-	private String targetType;
+    @Column(name = "TargetType")
+    private String targetType;
 
-	@Column(name = "ObjectUsage", columnDefinition = "TEXT")
-	private String objectUsage;
+    @Column(name = "ObjectUsage", columnDefinition = "TEXT")
+    private String objectUsage;
 
-	@Column(name = "IsQuery", columnDefinition = "BOOLEAN")
-	private boolean isQuery;
+    @Column(name = "IsQuery", columnDefinition = "BOOLEAN")
+    private boolean isQuery;
 
-	public String getWorkPeriod() {
-		return workPeriod;
-	}
+    public String getWorkPeriod() {
+        return workPeriod;
+    }
 
-	public String getEnclosingMethod() {
-		return enclosingMethod;
-	}
+    public String getEnclosingMethod() {
+        return enclosingMethod;
+    }
 
-	public Date getTimestamp() {
-		return timestamp;
-	}
+    public Date getTimestamp() {
+        return timestamp;
+    }
 
-	public String getTargetType() {
-		return targetType;
-	}
+    public String getTargetType() {
+        return targetType;
+    }
 }
