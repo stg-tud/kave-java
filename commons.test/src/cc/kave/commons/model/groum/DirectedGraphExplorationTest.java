8983e1ecc446db3dbf927674f0b036176cd0d13d
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2015-09-01 09:39:23 +0200

    Added a test to ensure behaviour of the graoh library.

---
+++ commons.test/src/cc/kave/commons/model/groum/DirectedGraphExplorationTest.java
@@ -0,0 +1,19 @@
+package cc.kave.commons.model.groum;
+
+import org.jgrapht.DirectedGraph;
+import org.jgrapht.graph.DefaultDirectedGraph;
+import org.jgrapht.graph.DefaultEdge;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DirectedGraphExplorationTest {
+
+	@Test
+	public void acceptsTwoEqualNodes() {
+		DirectedGraph<INode, DefaultEdge> g = new DefaultDirectedGraph<INode, DefaultEdge>(DefaultEdge.class);
+		g.addVertex(new TestNode("3"));
+		g.addVertex(new TestNode("3"));
+		assertEquals(2, g.vertexSet().size());
+	}
+}
