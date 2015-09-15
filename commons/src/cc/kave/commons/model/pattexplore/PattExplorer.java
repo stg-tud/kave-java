94b760f2234035ed0ba894787a1fa194abc326b8
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2015-09-15 13:43:32 +0200

    Changed internal data representation of PattExplorer.

--- commons/src/cc/kave/commons/model/pattexplore/PattExplorer.java
+++ commons/src/cc/kave/commons/model/pattexplore/PattExplorer.java
@@ -1,7 +1,9 @@
 package cc.kave.commons.model.pattexplore;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import cc.kave.commons.model.groum.Groum;
 import cc.kave.commons.model.groum.IGroum;
@@ -9,7 +11,6 @@ import cc.kave.commons.model.groum.SubGroum;
 import cc.kave.commons.model.groum.comparator.DFSGroumComparator;
 import cc.kave.commons.model.groum.comparator.HashCodeComparator;
 
-import com.google.common.collect.Multimap;
 import com.google.common.collect.TreeMultimap;
 
 public class PattExplorer {
@@ -19,62 +20,89 @@ public class PattExplorer {
 		this.threshold = threshold;
 	}
 
-	public List<SubGroum> explorePatterns(List<Groum> D) {
-		TreeMultimap<SubGroum, SubGroum> L = createGroumMap();
+	private static class SubGroumMultiSet {
+		private TreeMultimap<SubGroum, SubGroum> data = TreeMultimap.create(new DFSGroumComparator(),
+				new HashCodeComparator());
 
-		for (Groum groum : D) {
-			L.putAll(groum.getAtomicSubGroums());
+		public void add(SubGroum instance) {
+			data.put(instance, instance);
 		}
 
-		TreeMultimap<SubGroum, SubGroum> L2 = createGroumMap();
-		for (SubGroum groum : L.keySet()) {
-			if (getGroumFrequency(L, groum) >= threshold) {
-				L2.putAll(groum, L.get(groum));
+		public void addAll(Iterable<SubGroum> instances) {
+			instances.forEach(instance -> add(instance));
+		}
+
+		public void addAll(SubGroumMultiSet other) {
+			addAll(other.data.values());
+		}
+
+		public SubGroumMultiSet copy() {
+			SubGroumMultiSet clone = new SubGroumMultiSet();
+			clone.data.putAll(data);
+			return clone;
+		}
+
+		public Set<SubGroum> getPatterns() {
+			return new HashSet<>(data.keySet());
+		}
+
+		public int getPatternFrequency(SubGroum pattern) {
+			return data.get(pattern).size();
+		}
+
+		public Set<SubGroum> getPatternInstances(SubGroum pattern) {
+			return new HashSet<>(data.get(pattern));
+		}
+	}
+
+	public List<SubGroum> explorePatterns(List<Groum> D) {
+		SubGroumMultiSet L = new SubGroumMultiSet();
+
+		for (Groum groum : D) {
+			// TODO make getAtomicSubGroums return a SubGroumMultiSet
+			L.addAll(groum.getAtomicSubGroums().values());
+		}
+
+		SubGroumMultiSet L2 = new SubGroumMultiSet();
+		for (SubGroum pattern : L.getPatterns()) {
+			if (L.getPatternFrequency(pattern) >= threshold) {
+				L2.addAll(L.getPatternInstances(pattern));
 			}
 		}
 
-		TreeMultimap<SubGroum, SubGroum> explored = createGroumMap();
-		for (SubGroum pattern : L2.keySet()) {
-			explored.putAll(explore(pattern, L2, D));
+		SubGroumMultiSet explored = new SubGroumMultiSet();
+		for (SubGroum pattern : L2.getPatterns()) {
+			explored.addAll(explore(pattern, L2, D));
 		}
-		L2.putAll(explored);
+		L2.addAll(explored);
 
-		return new ArrayList<>(L2.keySet());
+		return new ArrayList<>(L2.getPatterns());
 
 	}
 
-	private int getGroumFrequency(TreeMultimap<SubGroum, SubGroum> L, SubGroum groum) {
-		return L.get(groum).size();
-	}
+	private SubGroumMultiSet explore(SubGroum P, SubGroumMultiSet L, List<Groum> D) {
+		SubGroumMultiSet patterns = L.copy();
+		SubGroumMultiSet newPatterns = new SubGroumMultiSet();
 
-	private TreeMultimap<SubGroum, SubGroum> createGroumMap() {
-		return TreeMultimap.create(new DFSGroumComparator(), new HashCodeComparator());
-	}
-
-	private Multimap<SubGroum, SubGroum> explore(SubGroum P, Multimap<SubGroum, SubGroum> L, List<Groum> D) {
-		TreeMultimap<SubGroum, SubGroum> patterns = createGroumMap();
-		Multimap<SubGroum, SubGroum> newPatterns = createGroumMap();
-
-		patterns.putAll(L);
-
-		for (IGroum U : L.keySet()) {
+		for (IGroum U : L.getPatterns()) {
 			if (U.getNodeCount() == 1) {
-				TreeMultimap<SubGroum, SubGroum> Q = createGroumMap();
+				SubGroumMultiSet Q = new SubGroumMultiSet();
 
-				for (SubGroum occurrence : patterns.get(P)) {
+				for (SubGroum occurrence : patterns.getPatternInstances(P)) {
 					List<SubGroum> candidates = occurrence.computeExtensions(U.getRoot());
 					for (SubGroum candidate : candidates) {
-						Q.put(candidate, candidate);
+						Q.add(candidate);
 					}
 				}
 
-				for (SubGroum candidate : Q.keySet()) {
-					if (getGroumFrequency(Q, candidate) >= threshold) {
-						newPatterns.putAll(candidate, Q.get(candidate));
-						patterns.putAll(candidate, Q.get(candidate));
+				for (SubGroum candidate : Q.getPatterns()) {
+					if (Q.getPatternFrequency(candidate) >= threshold) {
+						patterns.addAll(Q.getPatternInstances(candidate));
 
-						Multimap<SubGroum, SubGroum> explored = explore(candidate, patterns, D);
-						newPatterns.putAll(explored);
+						SubGroumMultiSet explored = explore(candidate, patterns, D);
+
+						newPatterns.addAll(Q.getPatternInstances(candidate));
+						newPatterns.addAll(explored);
 					}
 				}
 			}
