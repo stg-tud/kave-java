e8a276f42f2eae856f4a81234ca87327188a9af5
Ervina Cergani <cergani@st.informatik.tu-darmstadt.de>
2015-10-08 19:29:05 +0200

    few changes

--- cc.kave.episodes/src/cc/recommenders/mining/episodes/MaximalEpisodes.java
+++ cc.kave.episodes/src/cc/recommenders/mining/episodes/MaximalEpisodes.java
@@ -11,30 +11,68 @@
 package cc.recommenders.mining.episodes;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import cc.kave.commons.model.episodes.Episode;
 import cc.kave.commons.model.episodes.Fact;
 
-public class MaxFreqEpisodes {
+public class MaximalEpisodes {
 	
-	public Map<Integer, List<Episode>> getMaxFreqEpisodes(Map<Integer, List<Episode>> freqEpisodes) {
+	public Map<Integer, List<Episode>> getMaxFreqEpisodes(Map<Integer, List<Episode>> freqEpisodes) throws Exception {
 		Map<Integer, List<Episode>> finalEpisodes = new HashMap<Integer, List<Episode>>();
-		int numNodes = freqEpisodes.size();
 		
-		if (numNodes > 1) {
-			for (int node = 1; node < numNodes; node ++) {
+		Set<Integer> numberOfNodesSet = freqEpisodes.keySet();
+
+		int maximalNodeEpisode;
+		
+		if (!freqEpisodes.isEmpty()) {
+			maximalNodeEpisode = getMaximalNumberOfNodes(freqEpisodes);
+		} else {
+			throw new Exception("The list of learned episodes is empty!");
+		}
+		
+		
+		if (maximalNodeEpisode > 1) {
+			for (int node = 1; node < maximalNodeEpisode - 1; node ++) {
 				List<Episode> reducedList = reduceFreqEpisodes(freqEpisodes.get(node), freqEpisodes.get(node + 1));
 				if (reducedList.size() > 0) {
 					finalEpisodes.put(node, reducedList);
 				}
 			}
 		}
-		finalEpisodes.put(numNodes, freqEpisodes.get(numNodes));
+		finalEpisodes.put(maximalNodeEpisode, freqEpisodes.get(maximalNodeEpisode));
 		return finalEpisodes;
 	}
+	
+	public List<Episode> getIndex(Map<Integer, List<Episode>> episodes, int index) {
+		Set<Integer> numberOfNodesSet = episodes.keySet();
+		Iterator<Integer> itr = numberOfNodesSet.iterator();
+		int counter = 0;
+		int node = itr.next();
+		while (itr.hasNext()) {
+			node = itr.next();
+			if (counter == index) {
+				return episodes.get(node);
+			} else {
+				counter++;
+			}
+		}
+		return new LinkedList<Episode>();
+	}
+	
+	private int getMaximalNumberOfNodes(Map<Integer, List<Episode>> episodes) {
+		Set<Integer> numberOfNodesSet = episodes.keySet();
+		Iterator<Integer> itr = numberOfNodesSet.iterator();
+		int maximalNumberOfNode = itr.next();
+		while (itr.hasNext()) {
+			maximalNumberOfNode = itr.next();
+		}
+		return maximalNumberOfNode;
+	} 
 	
 	private List<Episode> reduceFreqEpisodes(List<Episode> episodeList1, List<Episode> episodeList2) {
 		List<Episode> finalList = new LinkedList<Episode>();
