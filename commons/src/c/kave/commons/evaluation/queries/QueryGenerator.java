dcb9c916f99640f059d9761f3cf35f60a9cc1767
Ervina Cergani <cergani@st.informatik.tu-darmstadt.de>
2015-12-14 11:59:11 +0100

    added QueryConfiguration enumerator

--- commons/src/c/kave/commons/evaluation/queries/QueryGenerator.java
+++ commons/src/c/kave/commons/evaluation/queries/QueryGenerator.java
@@ -1,28 +1,39 @@
 package c.kave.commons.evaluation.queries;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+
+import cc.kave.commons.model.episodes.Episode;
+import cc.kave.commons.model.episodes.QueryConfigurations;
 
 public class QueryGenerator {
 
-	// public Map<Method, Map<Integer, List<Query>>> createQuery(List<Method>
-	// allMethods, String configuration) {
-	// Map<Episode, Map<Integer, List<Episode>>> generatedQueries = new
-	// HashMap<Episode, Map<Integer, List<Episode>>>();
-	// Map<Integer, List<Episode>> skipedEventsAndQueryPair = new
-	// HashMap<Integer, List<Episode>>();
-	// List<Episode> currentueries = new LinkedList<Episode>();
-	// for (Method method : allMethods) {
-	// if (method.getNumberOfInvocations() > invocationsRemoved) {
-	// Query query = new Query();
-	// query.addFact(method.getMethodName());
-	// query.setNumberOfFacts(method.getNumberOfInvocations() + 1 -
-	// invocationsRemoved);
-	// }
-	// }
-	//
-	// return generatedQueries;
-	// }
+	public Map<Episode, Map<Integer, List<Episode>>> createQuery(List<Episode> allMethods,
+			QueryConfigurations configuration) {
+
+		for (Episode episode : allMethods) {
+		}
+		Map<Episode, Map<Integer, List<Episode>>> generatedQueries = new HashMap<Episode, Map<Integer, List<Episode>>>();
+		if (configuration.equals(QueryConfigurations.INCLUDEMD_REMOVEONEBYONE)) {
+			// generatedQueries =
+		}
+
+		// Map<Integer, List<Episode>> skipedEventsAndQueryPair = new
+		// HashMap<Integer, List<Episode>>();
+		// List<Episode> currentueries = new LinkedList<Episode>();
+		// for (Method method : allMethods) {
+		// if (method.getNumberOfInvocations() > invocationsRemoved) {
+		// Query query = new Query();
+		// query.addFact(method.getMethodName());
+		// query.setNumberOfFacts(method.getNumberOfInvocations() + 1 -
+		// invocationsRemoved);
+		// }
+		// }
+
+		return generatedQueries;
+	}
 
 	private List<int[]> subsetsGenerator(int[] array, int subsetLength) {
 		int N = array.length;
---
+++ commons/src/cc/kave/commons/model/episodes/QueryConfigurations.java
@@ -0,0 +1,5 @@
+package cc.kave.commons.model.episodes;
+
+public enum QueryConfigurations {
+	INCLUDEMD_REMOVEONEBYONE, INCLUDEMD_REMOVEBYPERCENTAGE, REMOVEMD_REMOVEONEBYONE, REMOVEMD_REMOVEBYPERCENTAGE
+}
