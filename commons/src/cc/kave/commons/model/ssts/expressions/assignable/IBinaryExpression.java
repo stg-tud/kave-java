59d2b420ccadd0066fb6213a46b777b42ee9f547
Sebastian Proksch <proksch@st.informatik.tu-darmstadt.de>
2015-12-14 09:21:04 +0100

    rebased on master

--- commons/src/cc/kave/commons/model/ssts/expressions/assignable/IBinaryExpression.java
+++ commons/src/cc/kave/commons/model/ssts/expressions/assignable/IBinaryExpression.java
@@ -1,10 +1,6 @@
 package cc.kave.commons.model.ssts.expressions.assignable;
 
-<<<<<<< HEAD
 import org.eclipse.jdt.annotation.NonNull;
-=======
-import javax.annotation.Nonnull;
->>>>>>> added Unary/Binary Expression to SST model
 
 import cc.kave.commons.model.ssts.expressions.IAssignableExpression;
 import cc.kave.commons.model.ssts.expressions.ISimpleExpression;
