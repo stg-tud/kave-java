e481e5d27d508da4c47eead784b6d4cd74502b50
Simon Reuß <simon.reuss@stud.tu-darmstadt.de>
2015-12-15 22:32:35 +0100

    Reactivated important classes and removed pruning of usages from the context
    Note that manual references to the separate recommenders repro are currently necessary for compilation.

--- exec/exec.pointsto/src/main/java/cc/kave/commons/pointsto/dummies/DummyDefinitionSite.java
+++ exec/exec.pointsto/src/main/java/cc/kave/commons/pointsto/dummies/DummyDefinitionSite.java
@@ -14,95 +14,95 @@ package cc.kave.commons.pointsto.dummies;
 
 import cc.kave.commons.model.names.FieldName;
 import cc.kave.commons.model.names.MethodName;
-//import cc.recommenders.usages.DefinitionSiteKind;
+import cc.recommenders.usages.DefinitionSiteKind;
 
 public class DummyDefinitionSite {
 
-//	private DefinitionSiteKind kind;
-//	private MethodName method;
-//	private FieldName field;
-//	private int argIndex = -1;
-//
-//	private DummyDefinitionSite() {
-//	}
-//
-//	public static DummyDefinitionSite byConstructor(MethodName method) {
-//		DummyDefinitionSite defSite = new DummyDefinitionSite();
-//		defSite.setKind(DefinitionSiteKind.NEW);
-//		defSite.setMethod(method);
-//		return defSite;
-//	}
-//
-//	public static DummyDefinitionSite byReturn(MethodName method) {
-//		DummyDefinitionSite defSite = new DummyDefinitionSite();
-//		defSite.setKind(DefinitionSiteKind.RETURN);
-//		defSite.setMethod(method);
-//		return defSite;
-//	}
-//
-//	public static DummyDefinitionSite byField(FieldName field) {
-//		DummyDefinitionSite defSite = new DummyDefinitionSite();
-//		defSite.setKind(DefinitionSiteKind.FIELD);
-//		defSite.setField(field);
-//		return defSite;
-//	}
-//
-//	public static DummyDefinitionSite byParam(MethodName method, int argIndex) {
-//		DummyDefinitionSite defSite = new DummyDefinitionSite();
-//		defSite.setKind(DefinitionSiteKind.PARAM);
-//		defSite.setMethod(method);
-//		defSite.setArgIndex(argIndex);
-//		return defSite;
-//	}
-//
-//	public static DummyDefinitionSite byThis() {
-//		DummyDefinitionSite defSite = new DummyDefinitionSite();
-//		defSite.setKind(DefinitionSiteKind.THIS);
-//		return defSite;
-//	}
-//
-//	public static DummyDefinitionSite byConstant() {
-//		DummyDefinitionSite defSite = new DummyDefinitionSite();
-//		defSite.setKind(DefinitionSiteKind.CONSTANT);
-//		return defSite;
-//	}
-//
-//	public static DummyDefinitionSite unknown() {
-//		DummyDefinitionSite defSite = new DummyDefinitionSite();
-//		defSite.setKind(DefinitionSiteKind.UNKNOWN);
-//		return defSite;
-//	}
-//
-//	public DefinitionSiteKind getKind() {
-//		return kind;
-//	}
-//
-//	public void setKind(DefinitionSiteKind kind) {
-//		this.kind = kind;
-//	}
-//
-//	public MethodName getMethod() {
-//		return method;
-//	}
-//
-//	public void setMethod(MethodName method) {
-//		this.method = method;
-//	}
-//
-//	public FieldName getField() {
-//		return field;
-//	}
-//
-//	public void setField(FieldName field) {
-//		this.field = field;
-//	}
-//
-//	public int getArgIndex() {
-//		return argIndex;
-//	}
-//
-//	public void setArgIndex(int argIndex) {
-//		this.argIndex = argIndex;
-//	}
+	private DefinitionSiteKind kind;
+	private MethodName method;
+	private FieldName field;
+	private int argIndex = -1;
+
+	private DummyDefinitionSite() {
+	}
+
+	public static DummyDefinitionSite byConstructor(MethodName method) {
+		DummyDefinitionSite defSite = new DummyDefinitionSite();
+		defSite.setKind(DefinitionSiteKind.NEW);
+		defSite.setMethod(method);
+		return defSite;
+	}
+
+	public static DummyDefinitionSite byReturn(MethodName method) {
+		DummyDefinitionSite defSite = new DummyDefinitionSite();
+		defSite.setKind(DefinitionSiteKind.RETURN);
+		defSite.setMethod(method);
+		return defSite;
+	}
+
+	public static DummyDefinitionSite byField(FieldName field) {
+		DummyDefinitionSite defSite = new DummyDefinitionSite();
+		defSite.setKind(DefinitionSiteKind.FIELD);
+		defSite.setField(field);
+		return defSite;
+	}
+
+	public static DummyDefinitionSite byParam(MethodName method, int argIndex) {
+		DummyDefinitionSite defSite = new DummyDefinitionSite();
+		defSite.setKind(DefinitionSiteKind.PARAM);
+		defSite.setMethod(method);
+		defSite.setArgIndex(argIndex);
+		return defSite;
+	}
+
+	public static DummyDefinitionSite byThis() {
+		DummyDefinitionSite defSite = new DummyDefinitionSite();
+		defSite.setKind(DefinitionSiteKind.THIS);
+		return defSite;
+	}
+
+	public static DummyDefinitionSite byConstant() {
+		DummyDefinitionSite defSite = new DummyDefinitionSite();
+		defSite.setKind(DefinitionSiteKind.CONSTANT);
+		return defSite;
+	}
+
+	public static DummyDefinitionSite unknown() {
+		DummyDefinitionSite defSite = new DummyDefinitionSite();
+		defSite.setKind(DefinitionSiteKind.UNKNOWN);
+		return defSite;
+	}
+
+	public DefinitionSiteKind getKind() {
+		return kind;
+	}
+
+	public void setKind(DefinitionSiteKind kind) {
+		this.kind = kind;
+	}
+
+	public MethodName getMethod() {
+		return method;
+	}
+
+	public void setMethod(MethodName method) {
+		this.method = method;
+	}
+
+	public FieldName getField() {
+		return field;
+	}
+
+	public void setField(FieldName field) {
+		this.field = field;
+	}
+
+	public int getArgIndex() {
+		return argIndex;
+	}
+
+	public void setArgIndex(int argIndex) {
+		this.argIndex = argIndex;
+	}
 
 }
