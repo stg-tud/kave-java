44fdc8560333d73d7f33dd7aac1acae79ef745ba
Carina Oberle <ca.oberle@gmail.com>
2016-02-08 15:59:53 +0100

    handle fall-through and breaks inside if statements

--- cc.kave.commons/src/main/java/cc/kave/commons/model/ssts/impl/transformation/switchblock/SwitchBlockNormalizationVisitor.java
+++ cc.kave.commons/src/main/java/cc/kave/commons/model/ssts/impl/transformation/switchblock/SwitchBlockNormalizationVisitor.java
@@ -17,46 +17,43 @@ package cc.kave.commons.model.ssts.impl.transformation.switchblock;
 
 import static cc.kave.commons.model.ssts.impl.SSTUtil.assign;
 import static cc.kave.commons.model.ssts.impl.SSTUtil.binExpr;
-import static cc.kave.commons.model.ssts.impl.SSTUtil.declare;
+import static cc.kave.commons.model.ssts.impl.SSTUtil.not;
 import static cc.kave.commons.model.ssts.impl.SSTUtil.refExpr;
 import static cc.kave.commons.model.ssts.impl.SSTUtil.switchBlock;
-import static cc.kave.commons.model.ssts.impl.SSTUtil.unaryExpr;
+import static cc.kave.commons.model.ssts.impl.transformation.BooleanDeclarationUtil.booleanDeclaration;
+import static cc.kave.commons.model.ssts.impl.transformation.BooleanDeclarationUtil.mainCondition;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import cc.kave.commons.model.names.csharp.TypeName;
+import com.google.common.collect.Lists;
+
 import cc.kave.commons.model.ssts.IStatement;
 import cc.kave.commons.model.ssts.blocks.ICaseBlock;
+import cc.kave.commons.model.ssts.blocks.IIfElseBlock;
 import cc.kave.commons.model.ssts.blocks.ISwitchBlock;
 import cc.kave.commons.model.ssts.declarations.IVariableDeclaration;
 import cc.kave.commons.model.ssts.expressions.ISimpleExpression;
 import cc.kave.commons.model.ssts.expressions.assignable.BinaryOperator;
-import cc.kave.commons.model.ssts.expressions.assignable.IBinaryExpression;
-import cc.kave.commons.model.ssts.expressions.assignable.UnaryOperator;
 import cc.kave.commons.model.ssts.expressions.simple.IReferenceExpression;
 import cc.kave.commons.model.ssts.impl.blocks.IfElseBlock;
-import cc.kave.commons.model.ssts.impl.statements.BreakStatement;
 import cc.kave.commons.model.ssts.impl.transformation.AbstractStatementNormalizationVisitor;
 import cc.kave.commons.model.ssts.references.IVariableReference;
 import cc.kave.commons.model.ssts.statements.IAssignment;
 import cc.kave.commons.model.ssts.statements.IBreakStatement;
 
-public class SwitchBlockNormalizationVisitor extends AbstractStatementNormalizationVisitor<IReferenceExpression> {
+public class SwitchBlockNormalizationVisitor
+		extends AbstractStatementNormalizationVisitor<SwitchBlockNormalizationContext> {
 
-	// private Map<IReferenceExpression, IBinaryExpression> refLookup;
-	private NodeFinderVisitor breakFinder;
 	private int counter;
 
 	public SwitchBlockNormalizationVisitor() {
 		counter = 0;
-		breakFinder = new NodeFinderVisitor(new BreakStatement());
-		// refLookup = new HashMap<IReferenceExpression, IBinaryExpression>();
 	}
 
 	@Override
-	public List<IStatement> visit(ISwitchBlock block, IReferenceExpression context) {
+	public List<IStatement> visit(ISwitchBlock block, SwitchBlockNormalizationContext context) {
 		// normalize inner switch blocks
 		super.visit(block, context);
 
@@ -65,114 +62,43 @@ public class SwitchBlockNormalizationVisitor extends AbstractStatementNormalizat
 		List<IStatement> defaultSection = block.getDefaultSection();
 
 		if (sections.isEmpty())
-			return resolveBreakStatements(defaultSection);
+			return normalizeEmptySwitch(block, context);
 
 		ICaseBlock section = sections.get(0);
 		ISimpleExpression label = section.getLabel();
 
-		boolean fallthrough = !containsBreak(section.getBody());
+		boolean fallthrough = fallthrough(section);
 		boolean fallthroughToDefault = fallthrough && sections.size() == 1;
 		boolean fallthroughToCase = fallthrough && !fallthroughToDefault;
 
-		List<IStatement> conditionStatements = getConditionStatements(ref, label, context);
-		IReferenceExpression condition = getMainCondition(conditionStatements);
-		IReferenceExpression updatedContext = fallthrough ? condition : null;
+		List<IStatement> conditionStatements = conditionDefinition(ref, label, context);
+		IReferenceExpression condition = mainCondition(conditionStatements);
+		List<IStatement> fallthroughCondition = new ArrayList<IStatement>();
 
-		List<ICaseBlock> elseSections = sections.subList(1, sections.size());
+		if (fallthrough)
+			fallthroughCondition = fallthroughConditionStatements(section, condition);
+
+		List<ICaseBlock> remainingSections = sections.subList(1, sections.size());
+		SwitchBlockNormalizationContext updatedContext = updatedContext(fallthroughCondition, condition, context,
+				fallthrough);
 		List<IStatement> thenPart = getThenPart(section);
-		List<IStatement> elsePart = getElsePart(elseSections, defaultSection, ref, updatedContext);
+		List<IStatement> remainingPart = getElsePart(remainingSections, defaultSection, ref, updatedContext);
 
-		List<IStatement> ifElseStatements = assembleIfElse(condition, thenPart, elsePart, fallthrough);
-		boolean emptyIf = ifElseStatements.isEmpty() || ifElseStatements.equals(elsePart);
-
+		List<IStatement> ifElseStatements = assembleIfElse(condition, thenPart, remainingPart, fallthrough);
+		boolean emptyIf = ifElseStatements.equals(remainingPart);
 		List<IStatement> normalized = new ArrayList<IStatement>();
-		
+
 		if (!emptyIf || fallthroughToCase) {
 			normalized.addAll(conditionStatements);
+			normalized.addAll(fallthroughCondition);
 		}
 		normalized.addAll(ifElseStatements);
 		return normalized;
+
 	}
-
-	// -------------------------- condition -----------------------------------
-
-	private String getConditionName() {
-		int count = counter;
-		counter++;
-		return "cond_" + count;
-	}
-
-	private IVariableDeclaration conditionDeclaration() {
-		// TODO: check TypeName
-		return declare(getConditionName(), TypeName.newTypeName("System.Boolean"));
-	}
-
-	private IReferenceExpression getMainCondition(List<IStatement> conditionStatements) {
-		List<IVariableDeclaration> vars = conditionStatements.stream().filter(s -> s instanceof IVariableDeclaration)
-				.map(v -> (IVariableDeclaration) v).collect(Collectors.toList());
-		return refExpr(vars.get(vars.size() - 1).getReference());
-	}
-
-	private List<IStatement> getConditionStatements(IVariableReference ref, ISimpleExpression label,
-			IReferenceExpression context) {
-		IVariableDeclaration eqVar = conditionDeclaration();
-		IVariableReference eqRef = eqVar.getReference();
-		IBinaryExpression eqExpr = binExpr(BinaryOperator.Equal, refExpr(ref), label);
-		IAssignment eqAssign = assign(eqRef, eqExpr);
-
-		List<IStatement> statements = new ArrayList<IStatement>();
-		statements.add(eqVar);
-		statements.add(eqAssign);
-
-		if (context != null) {
-			IVariableDeclaration orVar = conditionDeclaration();
-			IAssignment orAssign = assign(orVar.getReference(), binExpr(BinaryOperator.Or, context, refExpr(eqRef)));
-			statements.add(orVar);
-			statements.add(orAssign);
-		}
-
-		return statements;
-	}
-
-	// -------------------------- then part -----------------------------------
-
-	private List<IStatement> getThenPart(ICaseBlock section) {
-		return resolveBreakStatements(section.getBody());
-	}
-
-	// -------------------------- else part -----------------------------------
-
-	private List<IStatement> getElsePart(List<ICaseBlock> remainingSections, List<IStatement> defaultSection,
-			IVariableReference ref, IReferenceExpression context) {
-		return visit(switchBlock(ref, remainingSections, defaultSection), context);
-	}
-
-	// ------------------------ handle breaks ---------------------------------
-
-	private boolean containsBreak(List<IStatement> statements) {
-		for (IStatement s : statements) {
-			if (s.accept(breakFinder, null))
-				return true;
-		}
-		return false;
-	}
-
-	// TODO
-	private List<IStatement> resolveBreakStatements(List<IStatement> statements) {
-		int size = statements.size();
-		for (int i = 0; i < statements.size(); i++) {
-			if (statements.get(i) instanceof IBreakStatement) {
-				size = i;
-				break;
-			}
-		}
-		return statements.subList(0, size);
-	}
-
-	// --------------------------- helper -------------------------------------
 
 	private List<IStatement> assembleIfElse(IReferenceExpression condition, List<IStatement> thenPart,
-			List<IStatement> elsePart, boolean fallthrough) {
+			List<IStatement> remainingPart, boolean fallthrough) {
 		IfElseBlock ifElse = new IfElseBlock();
 		ifElse.setCondition(condition);
 		ifElse.setThen(thenPart);
@@ -180,12 +106,11 @@ public class SwitchBlockNormalizationVisitor extends AbstractStatementNormalizat
 		List<IStatement> statements = new ArrayList<IStatement>();
 		statements.add(ifElse);
 
-		/* in case of fallthrough, put remaining statements after if-block */
-		if (fallthrough) {
-			statements.addAll(elsePart);
-		} else {
-			ifElse.setElse(elsePart);
-		}
+		/* in case of fall-through, put remaining statements after if-block */
+		if (fallthrough)
+			statements.addAll(remainingPart);
+		else
+			ifElse.setElse(remainingPart);
 
 		boolean emptyThen = ifElse.getThen().isEmpty();
 		boolean emptyElse = ifElse.getElse().isEmpty();
@@ -201,17 +126,267 @@ public class SwitchBlockNormalizationVisitor extends AbstractStatementNormalizat
 		 * negate condition
 		 */
 		if (emptyThen) {
-			IVariableDeclaration negVar = conditionDeclaration();
-			IVariableReference negRef = negVar.getReference();
-			IAssignment negAssign = assign(negRef, unaryExpr(UnaryOperator.Not, condition));
-			statements.add(0, negVar);
-			statements.add(1, negAssign);
-			ifElse.setCondition(refExpr(negRef));
+			List<IStatement> negatedCond = negCond(condition);
+			statements.addAll(0, negatedCond);
+			ifElse.setCondition(mainCondition(negatedCond));
 			ifElse.setThen(ifElse.getElse());
 			ifElse.setElse(thenPart);
 		}
 
 		return statements;
+	}
+	
+	private List<IStatement> normalizeEmptySwitch(ISwitchBlock switchBlock, SwitchBlockNormalizationContext context) {
+		IReferenceExpression fallthroughCondition = context.getFallthroughCondition();
+		List<IReferenceExpression> labelConditions = context.getLabelConditions();
+		List<IStatement> defaultStatements = resolveBreakStatements(switchBlock.getDefaultSection());
+
+		boolean emptyDefault = defaultStatements.isEmpty();
+		boolean emptyLabels = labelConditions.isEmpty();
+		boolean conditionalFallthrough = context.getConditionalFallthrough();
+
+		if (emptyDefault || emptyLabels || !conditionalFallthrough)
+			return defaultStatements;
+
+		/*
+		 * Handle the special case where we fall through to the default section
+		 * under certain conditions. In this case we may neither put the default
+		 * part as else branch, nor simply append it to our list of statements.
+		 */
+		List<IStatement> normalized = new ArrayList<IStatement>();
+		ISimpleExpression condition = null;
+
+		/* conjunction of negated expressions */
+		for (IReferenceExpression cond : labelConditions) {
+			List<IStatement> negatedStmts = negCond(cond);
+			IReferenceExpression negatedCondition = mainCondition(negatedStmts);
+			normalized.addAll(negatedStmts);
+			if (condition == null) {
+				condition = negatedCondition;
+			} else {
+				List<IStatement> andStmts = andCond(condition, negatedCondition);
+				normalized.addAll(andStmts);
+				condition = mainCondition(andStmts);
+			}
+		}
+
+		List<IStatement> orStmts = orCond(fallthroughCondition, condition);
+		normalized.addAll(orStmts);
+
+		IfElseBlock ifBlock = new IfElseBlock();
+		ifBlock.setCondition(mainCondition(orStmts));
+		ifBlock.setThen(defaultStatements);
+		normalized.add(ifBlock);
+		return normalized;
+	}
+
+	// -------------------------- then part -----------------------------------
+
+	private List<IStatement> getThenPart(ICaseBlock section) {
+		return resolveBreakStatements(section.getBody());
+	}
+
+	// -------------------------- else part -----------------------------------
+
+	private List<IStatement> getElsePart(List<ICaseBlock> remainingSections, List<IStatement> defaultSection,
+			IVariableReference ref, SwitchBlockNormalizationContext context) {
+		return visit(switchBlock(ref, remainingSections, defaultSection), context);
+	}
+
+	// -------------------------- condition -----------------------------------
+
+	private IVariableDeclaration conditionDeclaration() {
+		return booleanDeclaration("$cond_" + counter++);
+	}
+
+	private List<IStatement> conditionDefinition(IVariableReference ref, ISimpleExpression label,
+			SwitchBlockNormalizationContext context) {
+		List<IStatement> statements = eqCond(refExpr(ref), label);
+		IReferenceExpression condition = mainCondition(statements);
+		IReferenceExpression fallthroughCondition = context.getFallthroughCondition();
+		context.addLabelCondition(condition);
+		if (fallthroughCondition != null)
+			statements.addAll(orCond(fallthroughCondition, condition));
+		return statements;
+	}
+
+	/**
+	 * Collect all conditions under which a fall-through occurs in the given
+	 * case block.
+	 */
+	private List<IStatement> fallthroughConditionStatements(ICaseBlock caseBlock, ISimpleExpression condition) {
+		return fallthroughConditionStatements(caseBlock.getBody(), condition);
+	}
+
+	private List<IStatement> fallthroughConditionStatements(List<IStatement> statements, ISimpleExpression condition) {
+		List<IStatement> res = new ArrayList<IStatement>();
+		List<IIfElseBlock> ifBlocks = statements.stream().filter(s -> s instanceof IIfElseBlock)
+				.map(s -> (IIfElseBlock) s).collect(Collectors.toList());
+
+		ISimpleExpression orCond = null;
+		for (IIfElseBlock ifBlock : ifBlocks) {
+			if (generalFallthrough(ifBlock))
+				continue;
+
+			ISimpleExpression collectedCondition = collectFallthroughCondition(ifBlock, res);
+			if (orCond == null) {
+				orCond = collectedCondition;
+			} else {
+				res.addAll(orCond(orCond, collectedCondition));
+				orCond = mainCondition(res);
+			}
+		}
+
+		if (orCond != null)
+			res.addAll(andCond(condition, orCond));
+		return res;
+	}
+
+	/**
+	 * Collect all conditions under which a fall-through occurs in a given if
+	 * block.
+	 */
+	private ISimpleExpression collectFallthroughCondition(IIfElseBlock ifBlock, List<IStatement> res) {
+		ISimpleExpression ifCondition = ifBlock.getCondition();
+		List<IStatement> thenPart = ifBlock.getThen();
+		List<IStatement> elsePart = ifBlock.getElse();
+
+		ISimpleExpression collectedCondition = null;
+		ISimpleExpression condThen = null;
+		ISimpleExpression condElse = null;
+
+		if (fallthrough(thenPart)) {
+			condThen = collectCondition(thenPart, ifCondition, res);
+			collectedCondition = condThen;
+		}
+
+		if (fallthrough(elsePart)) {
+			res.addAll(negCond(ifCondition));
+			condElse = collectCondition(elsePart, mainCondition(res), res);
+			collectedCondition = condElse;
+		}
+
+		if (condThen != null && condElse != null) {
+			res.addAll(orCond(condThen, condElse));
+			collectedCondition = mainCondition(res);
+		}
+
+		return collectedCondition;
+	}
+
+	private ISimpleExpression collectCondition(List<IStatement> statements, ISimpleExpression cond,
+			List<IStatement> res) {
+		List<IStatement> collected = fallthroughConditionStatements(statements, cond);
+		if (!collected.isEmpty()) {
+			res.addAll(collected);
+			res.addAll(andCond(cond, mainCondition(collected)));
+			cond = mainCondition(res);
+		}
+		return cond;
+	}
+
+	private List<IStatement> binCond(ISimpleExpression lhs, ISimpleExpression rhs, BinaryOperator op) {
+		IVariableDeclaration varDec = conditionDeclaration();
+		IAssignment varAssign = assign(varDec.getReference(), binExpr(op, lhs, rhs));
+		return Lists.newArrayList(varDec, varAssign);
+	}
+
+	private List<IStatement> eqCond(ISimpleExpression lhs, ISimpleExpression rhs) {
+		return binCond(lhs, rhs, BinaryOperator.Equal);
+	}
+
+	private List<IStatement> orCond(ISimpleExpression lhs, ISimpleExpression rhs) {
+		return binCond(lhs, rhs, BinaryOperator.Or);
+	}
+
+	private List<IStatement> andCond(ISimpleExpression lhs, ISimpleExpression rhs) {
+		return binCond(lhs, rhs, BinaryOperator.And);
+	}
+
+	private List<IStatement> negCond(ISimpleExpression cond) {
+		IVariableDeclaration varDec = conditionDeclaration();
+		IAssignment varAssign = assign(varDec.getReference(), not(cond));
+		return Lists.newArrayList(varDec, varAssign);
+	}
+
+	// -------------------------- fall-through --------------------------------
+
+	/**
+	 * Determine whether we have a fall-through under some condition.
+	 */
+	private boolean fallthrough(List<IStatement> statements) {
+		boolean outerFallthrough = statements.stream().noneMatch(s -> s instanceof IBreakStatement);
+		boolean innerFallthrough = true;
+
+		for (IStatement s : statements)
+			if (s instanceof IIfElseBlock)
+				innerFallthrough |= fallthrough((IIfElseBlock) s);
+
+		return outerFallthrough && innerFallthrough;
+	}
+
+	private boolean fallthrough(ICaseBlock section) {
+		return fallthrough(section.getBody());
+	}
+
+	private boolean fallthrough(IIfElseBlock ifBlock) {
+		return fallthrough(ifBlock.getThen()) || fallthrough(ifBlock.getElse());
+	}
+
+	/**
+	 * Determine whether we have a general fall-through (under *every* condition).
+	 */
+	private boolean generalFallthrough(List<IStatement> statements) {
+		boolean noOuterBreak = statements.stream().noneMatch(s -> s instanceof IBreakStatement);
+		boolean noInnerBreak = true;
+
+		for (IStatement s : statements)
+			if (s instanceof IIfElseBlock)
+				noInnerBreak &= generalFallthrough((IIfElseBlock) s);
+
+		return noOuterBreak && noInnerBreak;
+	}
+
+	private boolean generalFallthrough(IIfElseBlock ifBlock) {
+		return generalFallthrough(ifBlock.getThen()) && generalFallthrough(ifBlock.getElse());
+	}
+
+	// ------------------------ handle breaks ---------------------------------
+
+	private List<IStatement> resolveBreakStatements(List<IStatement> statements) {
+		List<IStatement> resolved = new ArrayList<IStatement>();
+
+		for (IStatement s : statements) {
+			if (s instanceof IBreakStatement)
+				break;
+			else if (s instanceof IfElseBlock) {
+				IfElseBlock ifElse = (IfElseBlock) s;
+				ifElse.setThen(resolveBreakStatements(ifElse.getThen()));
+				ifElse.setElse(resolveBreakStatements(ifElse.getElse()));
+				if (!isEmpty(ifElse))
+					resolved.add(ifElse);
+			} else
+				resolved.add(s);
+		}
+		return resolved;
+	}
+
+	// ----------------------- update context ---------------------------------
+
+	private SwitchBlockNormalizationContext updatedContext(List<IStatement> fallthroughCondition,
+			IReferenceExpression condition, SwitchBlockNormalizationContext context, boolean fallthrough) {
+		boolean noneCollected = fallthroughCondition.isEmpty();
+		boolean noConditionNecessary = !fallthrough;
+		IReferenceExpression collectedCondition = mainCondition(fallthroughCondition);
+		context.updateConditionalFallthrough(fallthrough, !noneCollected);
+		context.setFallthroughCondition(noConditionNecessary ? null : noneCollected ? condition : collectedCondition);
+		return context;
+	}
+
+	// --------------------------- helper -------------------------------------
+
+	private boolean isEmpty(IfElseBlock ifElse) {
+		return ifElse.getThen().isEmpty() && ifElse.getElse().isEmpty();
 	}
 
 }
