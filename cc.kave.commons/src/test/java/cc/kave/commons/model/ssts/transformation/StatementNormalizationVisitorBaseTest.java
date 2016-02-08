b3852844abba0e5eac448a31b5c68fb60f6bf63f
Carina Oberle <ca.oberle@gmail.com>
2016-02-08 16:06:35 +0100

    made StatementNormalizationVisitorBaseTest subclass of NormalizationVisitorBaseTest

--- cc.kave.commons/src/test/java/cc/kave/commons/model/ssts/transformation/StatementNormalizationVisitorBaseTest.java
+++ cc.kave.commons/src/test/java/cc/kave/commons/model/ssts/transformation/StatementNormalizationVisitorBaseTest.java
@@ -15,78 +15,31 @@
  */
 package cc.kave.commons.model.ssts.transformation;
 
-import static cc.kave.commons.model.ssts.impl.SSTUtil.declareMethod;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-
 import java.util.List;
-import java.util.Set;
 
 import org.junit.Before;
 
-import com.google.common.collect.Sets;
-
-import cc.kave.commons.model.ssts.ISST;
 import cc.kave.commons.model.ssts.IStatement;
-import cc.kave.commons.model.ssts.declarations.IMethodDeclaration;
-import cc.kave.commons.model.ssts.impl.SST;
 import cc.kave.commons.model.ssts.impl.SSTUtil;
-import cc.kave.commons.model.ssts.impl.transformation.AbstractStatementNormalizationVisitor;
-import cc.kave.commons.model.ssts.references.IVariableReference;
 
-public abstract class StatementNormalizationVisitorBaseTest<TReturn> {
-	protected AbstractStatementNormalizationVisitor<TReturn> sut;
-	protected SST sst, expectedSST;
+public abstract class StatementNormalizationVisitorBaseTest<TContext>
+		extends NormalizationVisitorBaseTest<TContext, List<IStatement>> {
 	protected IStatement stmt0, stmt1, stmt2, stmt3;
-	
+
 	@Before
 	public void setup() {
-		sst = new SST();
-		expectedSST = new SST();
-		
+		super.setup();
+
 		stmt0 = dummyStatement(0);
 		stmt1 = dummyStatement(1);
 		stmt2 = dummyStatement(2);
 		stmt3 = dummyStatement(3);
 	}
-	
-	// ---------------------------- helpers -----------------------------------
-	
-	protected void setNormalizing(IStatement... statements) {
-		sst.setMethods(Sets.newHashSet(declareMethod(statements)));
-	}
 
-	protected void setExpected(IStatement... statements) {
-		expectedSST.setMethods(Sets.newHashSet(declareMethod(statements)));
-	}
-	
+	// ---------------------------- helpers -----------------------------------
+
 	protected IStatement dummyStatement(int i) {
 		return SSTUtil.declareVar("dummy" + i);
 	}
-	
-	protected IVariableReference dummyVar(int i) {
-		return SSTUtil.variableReference("var" + i);
-	}
-	
-	// ---------------------------- asserts -----------------------------------
 
-	protected void assertSSTs(ISST expectedSST, ISST sst) {
-		Set<IMethodDeclaration> methods = sst.getMethods();
-		Set<IMethodDeclaration> expectedMethods = expectedSST.getMethods();
-		List<IStatement> body = methods.iterator().next().getBody();
-		List<IStatement> expectedBody = expectedMethods.iterator().next().getBody();
-
-		assertThat(methods.size(), equalTo(1));
-		assertThat(methods.size(), equalTo(expectedMethods.size()));
-		assertThat(body.size(), equalTo(expectedBody.size()));
-
-		for (int i = 0; i < body.size(); i++) {
-			assertThat(body.get(i), equalTo(expectedBody.get(i)));
-		}
-	}
-
-	protected void assertTransformedSST() {
-		sst.accept(sut, null);
-		assertSSTs(expectedSST, sst);
-	}
 }
