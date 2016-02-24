9ccc0e36437edbda09511b2b2f00dcc39dea73e8
Simon Reuß <simon.reuss@stud.tu-darmstadt.de>
2016-02-24 22:36:10 +0100

    UsageExtraction: Limit descending call depth to 10 in order to deal with rare 'monster' classes

--- exec/exec.pointsto/src/main/java/cc/kave/commons/pointsto/extraction/SimpleDescentStrategy.java
+++ exec/exec.pointsto/src/main/java/cc/kave/commons/pointsto/extraction/SimpleDescentStrategy.java
@@ -1,18 +1,15 @@
 /**
-* Copyright 2015 Simon Reuß
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* 	http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright 2015 Simon Reuß
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package cc.kave.commons.pointsto.extraction;
 
 import cc.kave.commons.model.ssts.declarations.IMethodDeclaration;
@@ -20,13 +17,18 @@ import cc.kave.commons.model.ssts.declarations.IPropertyDeclaration;
 import cc.kave.commons.pointsto.analysis.Callpath;
 
 /**
- * A {@link DescentStrategy} that descends only into non-entry point methods that are not yet contained in the current {@link Callpath}.
+ * A {@link DescentStrategy} that descends only into non-entry point methods that are not yet contained in the current
+ * {@link Callpath}.
  */
 public class SimpleDescentStrategy implements DescentStrategy {
 
+	private static final int MAX_CALL_DEPTH = 10;
+
 	@Override
 	public boolean descent(IMethodDeclaration methodDecl, Callpath currentCallpath) {
-		return !methodDecl.isEntryPoint() && !currentCallpath.contains(methodDecl.getName());
+		int currentCallDepth = currentCallpath.size();
+		return currentCallDepth < MAX_CALL_DEPTH && !methodDecl.isEntryPoint()
+				&& !currentCallpath.contains(methodDecl.getName());
 	}
 
 	@Override
