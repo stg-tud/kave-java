87ff073158dd42d88197377886ccec78bb9f9524
Sebastian Proksch <proksch@st.informatik.tu-darmstadt.de>
2016-04-19 22:52:22 +0200

    refactored WritingArchive and added tests

--- cc.kave.commons/src/main/java/cc/recommenders/io/WritingArchive.java
+++ cc.kave.commons/src/main/java/cc/recommenders/io/WritingArchive.java
@@ -10,55 +10,81 @@
  ******************************************************************************/
 package cc.recommenders.io;
 
-import java.io.Closeable;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Map;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
+import com.google.common.collect.Maps;
+
 import cc.kave.commons.utils.json.JsonUtils;
+import cc.recommenders.assertions.Asserts;
 
-public class WritingArchive implements Closeable {
+public class WritingArchive implements IWritingArchive {
 
-	private int count = 0;
-	private ZipOutputStream out;
+	private final File file;
+	private final Map<String, String> content;
 
 	public WritingArchive(File file) {
-		try {
-			out = new ZipOutputStream(new FileOutputStream(file));
-		} catch (FileNotFoundException e) {
-			throw new RuntimeException(e);
-		}
-	}
+		Asserts.assertFalse(file.exists());
+		File parent = file.getParentFile();
+		Asserts.assertTrue(parent.exists());
+		Asserts.assertTrue(parent.isDirectory());
 
-	public <T> void add(T obj) throws IOException {
-		String filename = count + ".json";
-		add(obj, filename);
-		count++;
-	}
-
-	public <T> void add(T obj, String filename) throws IOException {
-		out.putNextEntry(new ZipEntry(filename));
-		out.write(JsonUtils.toJson(obj).getBytes());
-		out.closeEntry();
-	}
-
-	public <T> void addPlain(String str) throws IOException {
-		String filename = count + ".txt";
-		addPlain(str, filename);
-		count++;
-	}
-
-	public <T> void addPlain(String str, String filename) throws IOException {
-		out.putNextEntry(new ZipEntry(filename));
-		out.write(str.getBytes());
-		out.closeEntry();
+		content = Maps.newLinkedHashMap();
+		this.file = file;
 	}
 
 	@Override
-	public void close() throws IOException {
-		out.close();
+	public <T> void addAll(Iterable<T> objs) {
+		for (T obj : objs) {
+			add(obj);
+		}
+	}
+
+	@Override
+	public <T> void add(T obj) {
+		int count = content.size();
+		String filename = count + ".json";
+		add(obj, filename);
+	}
+
+	@Override
+	public <T> void add(T obj, String fileName) {
+		addPlain(JsonUtils.toJson(obj), fileName);
+	}
+
+	@Override
+	public <T> void addPlain(String str) {
+		int count = content.size();
+		String filename = count + ".txt";
+		addPlain(str, filename);
+	}
+
+	@Override
+	public <T> void addPlain(String str, String fileName) {
+		// TODO crash on fileName exists
+		content.put(fileName, str);
+	}
+
+	@Override
+	public void close() {
+		try {
+			if (!content.isEmpty()) {
+				ZipOutputStream out = new ZipOutputStream(new FileOutputStream(file));
+				for (String fileName : content.keySet()) {
+					out.putNextEntry(new ZipEntry(fileName));
+					String json = content.get(fileName);
+					out.write(json.getBytes());
+					out.closeEntry();
+				}
+				out.close();
+				content.clear();
+			}
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
