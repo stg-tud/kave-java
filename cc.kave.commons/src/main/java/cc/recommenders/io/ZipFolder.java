d1e29148a4b86b1d48349872ac0e1d13f3411573
Sebastian Proksch <proksch@st.informatik.tu-darmstadt.de>
2016-04-19 12:46:21 +0200

    fixed all tests for ZipFolder

--- cc.kave.commons/src/main/java/cc/recommenders/io/ZipFolder.java
+++ cc.kave.commons/src/main/java/cc/recommenders/io/ZipFolder.java
@@ -15,15 +15,91 @@
  */
 package cc.recommenders.io;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Set;
+
+import javax.annotation.Nonnull;
+
+import org.apache.commons.io.FileUtils;
+
+import com.google.common.collect.Lists;
+
+import cc.recommenders.assertions.Asserts;
+
 public class ZipFolder {
 
-	public ZipFolder(String folderName, String json) {
-		// TODO Auto-generated constructor stub
+	public static final String MARKER_FILE_NAME = ".zipfolder";
+
+	private final String _root;
+	private final String _metaData;
+
+	private int _fileCounter;
+
+	public ZipFolder(@Nonnull String root) {
+		this(root, "");
+	}
+
+	public ZipFolder(@Nonnull String root, @Nonnull String metaData) {
+		File f = new File(root);
+		Asserts.assertTrue(f.exists() && f.isDirectory());
+		_root = root;
+		_metaData = metaData;
 	}
 
 	public WritingArchive createNewArchive() {
-		// TODO Auto-generated method stub
-		return null;
+		createMarker();
+		File fileName = createNextUnusedFileName();
+		return new WritingArchive(fileName);
 	}
 
+	private void createMarker() {
+		File markerName = new File(_root, MARKER_FILE_NAME);
+		if (!markerName.exists()) {
+			try {
+				FileUtils.writeStringToFile(markerName, _metaData);
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+
+	private File createNextUnusedFileName() {
+		File fileName = CreateNextFileName();
+		while (fileName.exists()) {
+			fileName = CreateNextFileName();
+		}
+		return fileName;
+	}
+
+	private File CreateNextFileName() {
+		return new File(_root, String.format("%d.zip", _fileCounter++));
+	}
+
+	public int getNumArchives() {
+		return findFiles().size();
+	}
+
+	private Set<String> findFiles() {
+		return new Directory(_root).findFiles(f -> f.endsWith(".zip"));
+	}
+
+	public List<ReadingArchive> openAll() {
+		List<ReadingArchive> archives = Lists.newLinkedList();
+		for (String zip : findFiles()) {
+			archives.add(open(zip));
+		}
+		return archives;
+	}
+
+	public ReadingArchive open(String relPath) {
+		try {
+			File f = Paths.get(_root, relPath).toFile();
+			return new ReadingArchive(f);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
--- cc.kave.commons/src/test/java/cc/recommenders/io/ZipFolderTest.java
+++ cc.kave.commons/src/test/java/cc/recommenders/io/ZipFolderTest.java
@@ -15,6 +15,130 @@
  */
 package cc.recommenders.io;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import cc.recommenders.exceptions.AssertionException;
+
 public class ZipFolderTest {
 
+	@Rule
+	public TemporaryFolder tmp = new TemporaryFolder();
+	private File _root;
+	private ZipFolder _sut;
+
+	@Before
+	public void setup() throws IOException {
+		_root = tmp.newFolder("data");
+		_sut = new ZipFolder(_root.getAbsolutePath());
+	}
+
+	@Test
+	public void FoldersAreMarkedOnCreationOfArchive() throws IOException {
+		File markerFile = file(_root, ZipFolder.MARKER_FILE_NAME);
+		assertFalse(markerFile.exists());
+
+		_sut.createNewArchive();
+
+		assertTrue(markerFile.exists());
+	}
+
+	@Test
+	public void FolderMarkersContainMetaData() throws IOException {
+		_sut = new ZipFolder(_root.getAbsolutePath(), "xyz");
+		_sut.createNewArchive();
+
+		String actual = FileUtils.readFileToString(file(_root, ZipFolder.MARKER_FILE_NAME));
+		assertEquals("xyz", actual);
+	}
+
+	@Test
+	public void ArchivesAreCreatedOnRequest() throws IOException {
+		assertEquals(0, _sut.getNumArchives());
+
+		try (WritingArchive wa = _sut.createNewArchive()) {
+			wa.add("a");
+			wa.add("b");
+		}
+
+		assertEquals(1, _sut.getNumArchives());
+
+		try (WritingArchive wa = _sut.createNewArchive()) {
+			wa.add("c");
+			wa.add("d");
+		}
+
+		assertEquals(2, _sut.getNumArchives());
+	}
+
+	@Test
+	public void NoFilesAreOverwritten() throws IOException {
+		file(_root, "0.zip").createNewFile();
+		file(_root, "1.zip").createNewFile();
+
+		assertEquals(2, _sut.getNumArchives());
+
+		try (WritingArchive wa = _sut.createNewArchive()) {
+			wa.add("a");
+		}
+
+		assertEquals(3, _sut.getNumArchives());
+	}
+
+	@Test
+	public void CorrectContentIsWrittenAndRead() throws IOException {
+		try (WritingArchive wa = _sut.createNewArchive()) {
+			wa.add("a");
+			wa.add("b");
+		}
+		try (WritingArchive wa = _sut.createNewArchive()) {
+			wa.add("c");
+			wa.add("d");
+		}
+
+		List<ReadingArchive> ras = _sut.openAll();
+		assertEquals(2, ras.size());
+
+		for (ReadingArchive ra : ras) {
+			List<String> entries = ra.getAll(String.class);
+
+			assertEquals(2, entries.size());
+
+			if (entries.contains("a")) {
+				assertTrue(entries.contains("b"));
+			} else {
+				assertTrue(entries.contains("c"));
+				assertTrue(entries.contains("d"));
+			}
+		}
+	}
+
+	@Test(expected = AssertionException.class)
+	public void DirectoryHasToExist() throws IOException {
+		File f = file(_root, "non", "existing", "folder");
+		new ZipFolder(f.getAbsolutePath());
+	}
+
+	@Test(expected = AssertionException.class)
+	public void DirectoryMustNotBeAFile() throws IOException {
+		File f = file(_root, "f");
+		f.createNewFile();
+		new ZipFolder(f.getAbsolutePath());
+	}
+
+	private File file(File dir, String... parts) throws IOException {
+		return Paths.get(dir.getAbsolutePath(), parts).toFile();
+	}
 }
