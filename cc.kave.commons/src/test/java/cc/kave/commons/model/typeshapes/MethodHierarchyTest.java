8aac340e39ced3dca3d88ebcfc43dc6bbee26e31
Sebastian Proksch <proksch@st.informatik.tu-darmstadt.de>
2016-04-22 13:07:53 +0200

    moved TypeShape tests to correct package

--- cc.kave.commons/src/test/java/cc/kave/commons/model/typeshapes/MethodHierarchyTest.java
+++ cc.kave.commons/src/test/java/cc/kave/commons/model/typeshapes/MethodHierarchyTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package cc.kave.commons.model.events.typeshapes;
+package cc.kave.commons.model.typeshapes;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.not;
