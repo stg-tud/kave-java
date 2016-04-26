055aef71ec2a3f9c3de10bd42f2f5a3387fce130
Ervina Cergani <cergani@st.informatik.tu-darmstadt.de>
2016-04-26 03:50:26 +0200

    fixed EventFilter bug

--- cc.kave.episodes/src/main/java/cc/kave/episodes/export/EventsFilter.java
+++ cc.kave.episodes/src/main/java/cc/kave/episodes/export/EventsFilter.java
@@ -21,20 +21,49 @@ import java.util.Map;
 import com.google.common.collect.Maps;
 
 import cc.kave.commons.model.episodes.Event;
+import cc.kave.commons.model.episodes.EventKind;
 import cc.kave.commons.model.episodes.Events;
 import cc.kave.commons.model.names.IMethodName;
 import cc.kave.commons.model.names.csharp.MethodName;
 import cc.recommenders.io.Logger;
 
-public class FilterEvents {
+public class EventsFilter {
 	
 	private static final String DUMMY_METHOD_NAME = "[You, Can] [Safely, Ignore].ThisDummyValue()";
 	private static final IMethodName DUMMY_METHOD = MethodName.newMethodName(DUMMY_METHOD_NAME);
 	public static final Event DUMMY_EVENT = Events.newContext(DUMMY_METHOD);
-
+	
 	public static StreamMapping filter(List<Event> stream) {
 		StreamMapping sm = new StreamMapping();
 		
+		Map<Event, Integer> occurrences = getFrequences(stream);
+		
+		int eventNumber = 0;
+		int sigletons = 0;
+		
+		sm.addEvent(DUMMY_EVENT);
+		for (Event e : stream) {
+			eventNumber++;
+			if (occurrences.get(e) > 1) {
+				sm.addEvent(e);
+			} else {
+				sigletons++;
+				if (e.getKind() == EventKind.METHOD_DECLARATION) {
+					sm.addEvent(Events.newHolder());
+				}
+			}
+			if (eventNumber % 100000 == 0){
+				Logger.log("Created mapping for %.5f", eventNumber/(stream.size()*1.0));
+			}
+		}
+		Logger.log("One time occured events are: %d", sigletons);
+		Logger.log("Number of unique events is: %d", occurrences.size());
+		Logger.log("Total number of events is: %d", stream.size());
+		
+		return sm;
+	}
+
+	private static Map<Event, Integer> getFrequences(List<Event> stream) {
 		Map<Event, Integer> occurrences = Maps.newHashMap();
 		int eventNumber = 0;
 		
@@ -50,24 +79,6 @@ public class FilterEvents {
 				Logger.log("Counting frequency for %d/%d", eventNumber, stream.size());
 			}
 		}
-		eventNumber = 0;
-		int singletons = 0;
-		
-		sm.addEvent(DUMMY_EVENT);
-		for (Event e : stream) {
-			if (occurrences.get(e) > 1) {
-				sm.addEvent(e);
-			} else {
-				singletons++;
-			}
-			if (eventNumber % 100000 == 0){
-				Logger.log("Created mapping for %d/%d", eventNumber, occurrences.size());
-			}
-		}
-		Logger.log("One time occured events are: %d", singletons);
-		Logger.log("Number of unique events is: %d", occurrences.size());
-		Logger.log("Total number of events is: %d", stream.size());
-		
-		return sm;
+		return occurrences;
 	}
 }
--- cc.kave.episodes/src/test/java/cc/kave/episodes/export/EventsFilterTest.java
+++ cc.kave.episodes/src/test/java/cc/kave/episodes/export/EventsFilterTest.java
@@ -15,9 +15,12 @@
  */
 package cc.kave.episodes.export;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.List;
 
 import org.junit.Before;
+import org.junit.Test;
 
 import com.google.common.collect.Lists;
 
@@ -26,17 +29,34 @@ import cc.kave.commons.model.episodes.Events;
 import cc.kave.commons.model.names.IMethodName;
 import cc.kave.commons.model.names.csharp.MethodName;
 
-public class FilterEventsTest {
+public class EventsFilterTest {
 
-	private List<Event> events;
+	private static final String DUMMY_METHOD_NAME = "[You, Can] [Safely, Ignore].ThisDummyValue()";
+	private static final IMethodName DUMMY_METHOD = MethodName.newMethodName(DUMMY_METHOD_NAME);
+	public static final Event DUMMY_EVENT = Events.newContext(DUMMY_METHOD);
 	
-	private FilterEvents sut;
+	private List<Event> events;
 	
 	@Before
 	public void setup() {
 		events = Lists.newArrayList(ctx(1), inv(2), inv(3), ctx(4), inv(5), inv(2), ctx(1), inv(3));
+	}
+	
+	@Test
+	public void filterTest() {
+		StreamMapping expected = new StreamMapping();
+		expected.addEvent(DUMMY_EVENT);
+		expected.addEvent(ctx(1));
+		expected.addEvent(inv(2));
+		expected.addEvent(inv(3));
+		expected.addEvent(hld());
+		expected.addEvent(inv(2));
+		expected.addEvent(ctx(1));
+		expected.addEvent(inv(3));
 		
-		sut = new FilterEvents();
+		StreamMapping actuals = EventsFilter.filter(events);
+		
+		assertEquals(expected, actuals);
 	}
 	
 	private static Event inv(int i) {
@@ -45,6 +65,10 @@ public class FilterEventsTest {
 
 	private static Event ctx(int i) {
 		return Events.newContext(m(i));
+	}
+	
+	private static Event hld() {
+		return Events.newHolder();
 	}
 	
 	private static IMethodName m(int i) {
