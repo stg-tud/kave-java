9ea2165dfb36b15a9eed105989eaeed05da7ce54
Ervina Cergani <cergani@st.informatik.tu-darmstadt.de>
2016-06-14 16:16:59 +0200

    added ReductionByRepos class and tests for stream reduction

---
+++ cc.kave.episodes/src/main/java/cc/kave/episodes/aastart/frameworks/ReductionByRepos.java
@@ -0,0 +1,108 @@
+/**
+ * Copyright 2016 Technische Universität Darmstadt
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * Copyright 2016 Technische Universität Darmstadt
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cc.kave.episodes.aastart.frameworks;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+import java.util.zip.ZipException;
+
+import com.google.common.base.Predicate;
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
+
+import cc.kave.commons.model.episodes.Event;
+import cc.kave.commons.model.events.completionevents.Context;
+import cc.kave.episodes.export.EventStreamGenerator;
+import cc.recommenders.io.Directory;
+import cc.recommenders.io.Logger;
+import cc.recommenders.io.ReadingArchive;
+
+public class ReductionByRepos {
+
+	private Directory contextsDir;
+
+	@Inject
+	public ReductionByRepos(@Named("contexts") Directory directory) {
+		this.contextsDir = directory;
+	}
+
+	public List<Event> select(int numberOfRepos) throws ZipException, IOException {
+		EventStreamGenerator generator = new EventStreamGenerator();
+		String repoName = "";
+		int repoID = 0;
+
+		for (String zip : findZips()) {
+			Logger.log("Reading zip file %s", zip.toString());
+			if (repoName.equalsIgnoreCase("")) {
+				repoName = getRepoName(zip);
+				repoID++;
+			} else if (!zip.startsWith(repoName)) {
+				repoName = getRepoName(zip);
+				repoID++;
+			}
+
+			if (repoID > numberOfRepos) {
+				break;
+			}
+			ReadingArchive ra = contextsDir.getReadingArchive(zip);
+
+			while (ra.hasNext()) {
+				Context ctx = ra.getNext(Context.class);
+				if (ctx == null) {
+					continue;
+				}
+				generator.add(ctx);
+			}
+			ra.close();
+		}
+		List<Event> allEvents = generator.getEventStream();
+		return allEvents;
+	}
+
+	private String getRepoName(String zipName) {
+		int index = zipName.indexOf("/", zipName.indexOf("/", zipName.indexOf("/") + 1) + 1);
+		String startPrefix = zipName.substring(0, index);
+
+		return startPrefix;
+	}
+
+	private Set<String> findZips() {
+		Set<String> zips = contextsDir.findFiles(new Predicate<String>() {
+
+			@Override
+			public boolean apply(String arg0) {
+				return arg0.endsWith(".zip");
+			}
+		});
+		return zips;
+	}
+}
---
+++ cc.kave.episodes/src/test/java/cc/kave/episodes/aastart/frameworks/ReductionByReposTest.java
@@ -0,0 +1,265 @@
+/**
+ * Copyright 2016 Technische Universität Darmstadt
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cc.kave.episodes.aastart.frameworks;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.zip.ZipException;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.rules.TemporaryFolder;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import com.google.common.base.Predicate;
+import com.google.common.collect.Maps;
+
+import cc.kave.commons.model.episodes.Event;
+import cc.kave.commons.model.episodes.Events;
+import cc.kave.commons.model.events.completionevents.Context;
+import cc.kave.commons.model.names.IMethodName;
+import cc.kave.commons.model.names.csharp.MethodName;
+import cc.kave.commons.model.ssts.impl.SST;
+import cc.kave.commons.model.ssts.impl.blocks.DoLoop;
+import cc.kave.commons.model.ssts.impl.declarations.MethodDeclaration;
+import cc.kave.commons.model.ssts.impl.expressions.assignable.InvocationExpression;
+import cc.kave.commons.model.ssts.impl.statements.ContinueStatement;
+import cc.kave.commons.model.ssts.impl.statements.ExpressionStatement;
+import cc.kave.episodes.export.EventStreamGenerator;
+import cc.kave.episodes.export.EventStreamIo;
+import cc.recommenders.io.Directory;
+import cc.recommenders.io.Logger;
+import cc.recommenders.io.ReadingArchive;
+
+public class ReductionByReposTest {
+
+	@Rule
+	public TemporaryFolder rootFolder = new TemporaryFolder();
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+
+	@Mock
+	private Directory rootDirectory;
+	@Mock
+	private EventStreamGenerator generator;
+	@Mock
+	private EventStreamIo streamer;
+	@Mock
+	private FileUtils fileUtils;
+
+	private static final String REPO1 = "Github/usr1/repo1/ws.zip";
+	private static final String REPO3 = "Github/usr1/repo3/ws.zip";
+	private static final String REPO2 = "Github/usr1/repo2/ws.zip";
+	private static final int NUMBEROFREPOS = 2;
+
+	private Map<String, Context> data;
+	private Map<String, ReadingArchive> ras;
+
+	private ReductionByRepos sut;
+
+	@Before
+	public void setup() throws IOException {
+		Logger.reset();
+		Logger.setCapturing(true);
+
+		MockitoAnnotations.initMocks(this);
+
+		data = Maps.newLinkedHashMap();
+		ras = Maps.newLinkedHashMap();
+		sut = new ReductionByRepos(rootDirectory);
+
+		SST sst = new SST();
+		MethodDeclaration md = new MethodDeclaration();
+		md.setName(MethodName.newMethodName("[T,P] [T2,P].M()"));
+		md.getBody().add(new ContinueStatement());
+		sst.getMethods().add(md);
+
+		MethodDeclaration md2 = new MethodDeclaration();
+		md2.setName(MethodName.newMethodName("[T,P] [T3,P].M2()"));
+
+		InvocationExpression ie1 = new InvocationExpression();
+		IMethodName methodName = MethodName.newMethodName("[System.Void, mscore, 4.0.0.0] [T, P, 1.2.3.4].MI1()");
+		ie1.setMethodName(methodName);
+		InvocationExpression ie2 = new InvocationExpression();
+		IMethodName methodName2 = MethodName.newMethodName("[System.Void, mscore, 4.0.0.0] [T, P, 1.2.3.4].MI2()");
+		ie2.setMethodName(methodName2);
+
+		md2.getBody().add(wrap(ie1));
+		md2.getBody().add(wrap(ie2));
+		md2.getBody().add(wrap(ie2));
+		sst.getMethods().add(md2);
+
+		Context context = new Context();
+		context.setSST(sst);
+		data.put(REPO1, context);
+
+		SST sst3 = new SST();
+		MethodDeclaration md3 = new MethodDeclaration();
+		md3.setName(MethodName.newMethodName("[T,P] [T2,P].M()"));
+		InvocationExpression ie5 = new InvocationExpression();
+		IMethodName methodName5 = MethodName.newMethodName("[System.Void,mscore, 4.0.0.0] [T, P, 1.2.3.4].MI1()");
+		ie5.setMethodName(methodName5);
+		md3.getBody().add(wrap(ie5));
+		sst3.getMethods().add(md3);
+		
+		Context context3 = new Context();
+		context3.setSST(sst3);
+		data.put(REPO3, context3);
+
+		when(rootDirectory.findFiles(anyPredicateOf(String.class))).thenAnswer(new Answer<Set<String>>() {
+			@Override
+			public Set<String> answer(InvocationOnMock invocation) throws Throwable {
+				return data.keySet();
+			}
+		});
+		when(rootDirectory.getReadingArchive(anyString())).then(new Answer<ReadingArchive>() {
+			@Override
+			public ReadingArchive answer(InvocationOnMock invocation) throws Throwable {
+				String file = (String) invocation.getArguments()[0];
+				ReadingArchive ra = mock(ReadingArchive.class);
+				ras.put(file, ra);
+				when(ra.hasNext()).thenReturn(true).thenReturn(false);
+				when(ra.getNext(Context.class)).thenReturn(data.get(file));
+				return ra;
+			}
+		});
+		Logger.setPrinting(false);
+	}
+
+	@After
+	public void teardown() {
+		Logger.reset();
+	}
+
+	@Test
+	public void contextTest() throws ZipException, IOException {
+		sut.select(NUMBEROFREPOS);
+
+		verify(rootDirectory).findFiles(anyPredicateOf(String.class));
+		verify(rootDirectory).getReadingArchive(REPO1);
+		verify(rootDirectory).getReadingArchive(REPO3);
+		verify(ras.get(REPO1), times(2)).hasNext();
+		verify(ras.get(REPO1)).getNext(Context.class);
+		verify(ras.get(REPO3), times(2)).hasNext();
+		verify(ras.get(REPO3)).getNext(Context.class);
+	}
+
+	@Test
+	public void readTwoArchives() throws IOException {
+		SST sst = new SST();
+		MethodDeclaration md3 = new MethodDeclaration();
+		md3.setName(MethodName.newMethodName("[T,P] [T2,P].M3()"));
+		md3.getBody().add(new DoLoop());
+
+		InvocationExpression ie3 = new InvocationExpression();
+		IMethodName methodName = MethodName.newMethodName("[System.Void, mscore, 4.0.0.0] [T, P, 1.2.3.4].MI3()");
+		ie3.setMethodName(methodName);
+
+		InvocationExpression ie4 = new InvocationExpression();
+		methodName = MethodName.newMethodName("[System.Void, mscore, 4.0.0.0] [T, P, 1.2.3.4].MI3()");
+		ie4.setMethodName(methodName);
+
+		md3.getBody().add(wrap(ie3));
+		md3.getBody().add(wrap(ie4));
+		md3.getBody().add(new ExpressionStatement());
+
+		sst.getMethods().add(md3);
+		Context context = new Context();
+		context.setSST(sst);
+
+		data.put(REPO2, context);
+
+		List<Event> actualEvents = sut.select(NUMBEROFREPOS);
+		List<Event> exoectedEvents = getExpectedEvents();
+		
+		verify(rootDirectory).findFiles(anyPredicateOf(String.class));
+		verify(rootDirectory).getReadingArchive(REPO1);
+		verify(rootDirectory).getReadingArchive(REPO3);
+//		verify(rootDirectory).getReadingArchive(REPO2);
+
+		verify(ras.get(REPO1), times(2)).hasNext();
+		verify(ras.get(REPO1)).getNext(Context.class);
+		verify(ras.get(REPO3), times(2)).hasNext();
+		verify(ras.get(REPO3)).getNext(Context.class);
+//		verify(ras.get(REPO2), times(2)).hasNext();
+//		verify(ras.get(REPO2)).getNext(Context.class);
+		
+		assertEquals(exoectedEvents.size(), actualEvents.size());
+		assertEquals(exoectedEvents, actualEvents);
+	}
+
+	private <T> Predicate<T> anyPredicateOf(Class<T> clazz) {
+		@SuppressWarnings("unchecked")
+		Predicate<T> p = any(Predicate.class);
+		return p;
+	}
+
+	private static ExpressionStatement wrap(InvocationExpression ie1) {
+		ExpressionStatement expressionStatement = new ExpressionStatement();
+		expressionStatement.setExpression(ie1);
+		return expressionStatement;
+	}
+
+	private List<Event> getExpectedEvents() {
+		List<Event> events = new LinkedList<Event>();
+		
+		String md1 = "[?] [?].???()";
+		IMethodName methodDecl1 = MethodName.newMethodName(md1);
+		Event e1 = Events.newContext(methodDecl1);
+		events.add(e1);
+		
+		String inv1 = "[System.Void, mscore, 4.0.0.0] [T, P, 1.2.3.4].MI1()";
+		IMethodName methodInv1 = MethodName.newMethodName(inv1);
+		Event e2 = Events.newInvocation(methodInv1);
+		events.add(e2);
+
+		String inv2 = "[System.Void, mscore, 4.0.0.0] [T, P, 1.2.3.4].MI2()";
+		IMethodName methodInv2 = MethodName.newMethodName(inv2);
+		Event e3 = Events.newInvocation(methodInv2);
+		events.add(e3);
+		events.add(e3);
+		
+		String md2 = "[?] [?].???()";
+		IMethodName methodDecl2 = MethodName.newMethodName(md2);
+		Event e4 = Events.newContext(methodDecl2);
+		events.add(e4);
+		
+		String inv3 = "[System.Void,mscore, 4.0.0.0] [T, P, 1.2.3.4].MI1()";
+		IMethodName methodInv3 = MethodName.newMethodName(inv3);
+		Event e5 = Events.newInvocation(methodInv3);
+		events.add(e5);
+
+		return events;
+	}
+}
