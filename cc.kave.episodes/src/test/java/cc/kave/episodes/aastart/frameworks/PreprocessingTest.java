41616ed3109d5049c6c52b6f3fa588f3102a0f9d
Ervina Cergani <cergani@st.informatik.tu-darmstadt.de>
2016-06-22 15:05:57 +0200

    refactored Preprocessing class

--- cc.kave.episodes/src/test/java/cc/kave/episodes/aastart/frameworks/PreprocessingTest.java
+++ cc.kave.episodes/src/test/java/cc/kave/episodes/aastart/frameworks/PreprocessingTest.java
@@ -19,14 +19,13 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyInt;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.zip.ZipException;
 
 import org.apache.commons.io.FileUtils;
@@ -39,21 +38,14 @@ import org.junit.rules.TemporaryFolder;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import com.google.common.base.Predicate;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 
 import cc.kave.commons.model.episodes.Event;
 import cc.kave.commons.model.episodes.Events;
-import cc.kave.commons.model.events.completionevents.Context;
 import cc.kave.commons.model.names.IMethodName;
 import cc.kave.commons.model.names.csharp.MethodName;
-import cc.kave.commons.model.ssts.impl.SST;
-import cc.kave.commons.model.ssts.impl.blocks.DoLoop;
-import cc.kave.commons.model.ssts.impl.declarations.MethodDeclaration;
-import cc.kave.commons.model.ssts.impl.expressions.assignable.InvocationExpression;
-import cc.kave.commons.model.ssts.impl.statements.ExpressionStatement;
 import cc.kave.episodes.export.EventStreamIo;
-import cc.kave.episodes.export.EventsFilter;
 import cc.kave.episodes.model.EventStream;
 import cc.recommenders.exceptions.AssertionException;
 import cc.recommenders.io.Directory;
@@ -70,9 +62,12 @@ public class PreprocessingTest {
 	private Directory rootDirectory;
 	@Mock
 	private ReductionByRepos repos;
+	
+	private static final int NUMBREPOS = 10;
 
 	private List<Event> events;
 	private EventStream stream;
+	private Map<Event, Integer> frequencies;
 
 	private Preprocessing sut;
 
@@ -93,10 +88,16 @@ public class PreprocessingTest {
 		stream.addEvent(ctx(1));
 		stream.addEvent(inv(3));
 		
+		frequencies = Maps.newHashMap();
+		frequencies.put(ctx(1), 2);
+		frequencies.put(inv(2), 2);
+		frequencies.put(inv(3), 2);
+		frequencies.put(ctx(4), 1);
+		frequencies.put(inv(5), 1);
+		
 		sut = new Preprocessing(rootDirectory, rootFolder.getRoot(), repos);
 
 		when(repos.select(any(Directory.class), anyInt())).thenReturn(events);
-		when(EventsFilter.filterStream(any(List.class), anyInt())).thenReturn(stream);
 
 		Logger.setPrinting(false);
 	}
@@ -105,14 +106,7 @@ public class PreprocessingTest {
 	public void teardown() {
 		Logger.reset();
 	}
-
-	@Test
-	public void contextTest() throws ZipException, IOException {
-		sut.generate();;
-
-//		verify(rootDirectory, times(2)).findFiles(anyPredicateOf(String.class));
-	}
-
+	
 	@Test
 	public void cannotBeInitializedWithNonExistingFolder() {
 		thrown.expect(AssertionException.class);
@@ -129,91 +123,61 @@ public class PreprocessingTest {
 	}
 
 	@Test
-	public void readTwoArchives() throws IOException {
-		SST sst = new SST();
-		MethodDeclaration md3 = new MethodDeclaration();
-		md3.setName(MethodName.newMethodName("[T,P] [T2,P].M3()"));
-		md3.getBody().add(new DoLoop());
+	public void mockIsCalled() throws ZipException, IOException {
+		sut.generate();
 
-		InvocationExpression ie3 = new InvocationExpression();
-		IMethodName methodName = MethodName.newMethodName("[System.Void, mscore, 4.0.0.0] [T, P, 1.2.3.4].MI3()");
-		ie3.setMethodName(methodName);
+		verify(repos).select(any(Directory.class), anyInt());
+	}
 
-		md3.getBody().add(wrap(ie3));
+	@Test
+	public void filesAreCreated() throws IOException {
+		sut.generate();
 
-		InvocationExpression ie4 = new InvocationExpression();
-		methodName = MethodName.newMethodName("[System.Void, mscore, 4.0.0.0] [T, P, 1.2.3.4].MI3()");
-		ie4.setMethodName(methodName);
-
-		md3.getBody().add(wrap(ie4));
-
-		md3.getBody().add(new ExpressionStatement());
-
-		sst.getMethods().add(md3);
-		Context context = new Context();
-		context.setSST(sst);
-
-
-		sut.generate();;
-
-		verify(rootDirectory, times(2)).findFiles(anyPredicateOf(String.class));
+		verify(repos).select(any(Directory.class), anyInt());
 
 		File streamFile = new File(getStreamPath());
 		File mappingFile = new File(getMappingPath());
 
-		String expectedStream = "2,0.500\n2,0.501\n3,1.002\n3,1.003\n";
+		assertTrue(streamFile.exists());
+		assertTrue(mappingFile.exists());
+	}
+	
+	@Test
+	public void contentTest() throws IOException {
+		sut.generate();
 
-		// ctx1 ctx2 inv1 inv2 inv2 ctx2 inv3 inv3
+		verify(repos).select(any(Directory.class), anyInt());
+
+		File streamFile = new File(getStreamPath());
+		File mappingFile = new File(getMappingPath());
+		
+		// ctx(1), inv(2), inv(3), ctx(4), inv(5), inv(2), ctx(1), inv(3)
+		String expectedStream = "1,0.500\n2,0.501\n3,0.502\n2,1.003\n1,1.504\n3,1.505\n";
+		
+		List<Event> expectedMapping = Lists.newLinkedList();
+		expectedMapping.add(Events.newDummyEvent());
+		expectedMapping.add(ctx(1));
+		expectedMapping.add(inv(2));
+		expectedMapping.add(inv(3));
+		expectedMapping.add(Events.newUnknownEvent());
+		
 		String actualStream = FileUtils.readFileToString(streamFile);
 		List<Event> actualMapping = EventStreamIo.readMapping(mappingFile.getAbsolutePath());
 		
 		assertEquals(expectedStream, actualStream);
-		assertEquals(expectedMapping(), actualMapping);
-		
-		assertTrue(streamFile.exists());
-		assertTrue(mappingFile.exists());
-	}
-
-	private <T> Predicate<T> anyPredicateOf(Class<T> clazz) {
-		@SuppressWarnings("unchecked")
-		Predicate<T> p = any(Predicate.class);
-		return p;
-	}
-
-	private static ExpressionStatement wrap(InvocationExpression ie1) {
-		ExpressionStatement expressionStatement = new ExpressionStatement();
-		expressionStatement.setExpression(ie1);
-		return expressionStatement;
+		assertEquals(expectedMapping, actualMapping);
 	}
 
 	private String getStreamPath() {
-		File streamFile = new File(rootFolder.getRoot().getAbsolutePath() + "/eventStream.txt");
+		File streamFile = new File(rootFolder.getRoot().getAbsolutePath() + "/" + NUMBREPOS + "Repos/stream.txt");
 		return streamFile.getAbsolutePath();
 	}
 
 	private String getMappingPath() {
-		File streamFile = new File(rootFolder.getRoot().getAbsolutePath() + "/eventMapping.txt");
+		File streamFile = new File(rootFolder.getRoot().getAbsolutePath() + "/" + NUMBREPOS + "Repos/mapping.txt");
 		return streamFile.getAbsolutePath();
 	}
 
-	private List<Event> expectedMapping() {
-		List<Event> events = new LinkedList<Event>();
-		events.add(Events.newDummyEvent());
-		events.add(Events.newUnknownEvent());
-		
-		String inv1 = "[System.Void, mscore, 4.0.0.0] [T, P, 1.2.3.4].MI2()";
-		IMethodName methodInv1 = MethodName.newMethodName(inv1);
-		Event e2 = Events.newInvocation(methodInv1);
-		events.add(e2);
-		
-		String inv2 = "[System.Void, mscore, 4.0.0.0] [T, P, 1.2.3.4].MI3()";
-		IMethodName methodInv2 = MethodName.newMethodName(inv2);
-		Event e3 = Events.newInvocation(methodInv2);
-		events.add(e3);
-		
-		return events;
-	}
-	
 	private static Event inv(int i) {
 		return Events.newInvocation(m(i));
 	}
