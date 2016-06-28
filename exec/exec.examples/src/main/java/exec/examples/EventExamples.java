203427471cc09c89224512a5b6eead1012ab9e64
Sebastian Proksch <proksch@st.informatik.tu-darmstadt.de>
2016-06-28 16:14:25 +0200

    extended example for working with the enriched event stream

--- exec/exec.examples/src/main/java/exec/examples/EventExamples.java
+++ exec/exec.examples/src/main/java/exec/examples/EventExamples.java
@@ -16,35 +16,14 @@
 package exec.examples;
 
 import java.io.File;
-import java.util.Date;
 import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 
 import com.google.common.collect.Lists;
 
-import cc.kave.commons.model.events.ActivityEvent;
 import cc.kave.commons.model.events.CommandEvent;
-import cc.kave.commons.model.events.ErrorEvent;
-import cc.kave.commons.model.events.IDEEvent;
 import cc.kave.commons.model.events.IIDEEvent;
-import cc.kave.commons.model.events.InfoEvent;
-import cc.kave.commons.model.events.NavigationEvent;
-import cc.kave.commons.model.events.SystemEvent;
-import cc.kave.commons.model.events.completionevents.CompletionEvent;
-import cc.kave.commons.model.events.testrunevents.TestRunEvent;
-import cc.kave.commons.model.events.userprofiles.UserProfileEvent;
-import cc.kave.commons.model.events.versioncontrolevents.VersionControlEvent;
-import cc.kave.commons.model.events.visualstudio.BuildEvent;
-import cc.kave.commons.model.events.visualstudio.DebuggerEvent;
-import cc.kave.commons.model.events.visualstudio.DocumentEvent;
-import cc.kave.commons.model.events.visualstudio.EditEvent;
-import cc.kave.commons.model.events.visualstudio.FindEvent;
-import cc.kave.commons.model.events.visualstudio.IDEStateEvent;
-import cc.kave.commons.model.events.visualstudio.InstallEvent;
-import cc.kave.commons.model.events.visualstudio.SolutionEvent;
-import cc.kave.commons.model.events.visualstudio.UpdateEvent;
-import cc.kave.commons.model.events.visualstudio.WindowEvent;
 import cc.kave.commons.utils.json.JsonUtils;
 import cc.recommenders.io.ReadingArchive;
 
@@ -63,100 +42,80 @@ public class EventExamples {
 	 */
 	private static final String DIR_USERDATA = "/Users/seb/Downloads/All-Clean/";
 
-	public static void main(String[] args) {
-
-		System.out.println(new Date());
-
-		List<String> userZips = findAllUsers(DIR_USERDATA);
-
-		for (String user : userZips) {
-			System.out.println("### " + user);
-			List<String> eventStrings = read(user);
-			for (String eventString : eventStrings) {
-
-				IDEEvent event = JsonUtils.fromJson(eventString, IIDEEvent.class);
-
-				if (event instanceof ActivityEvent) {
-					System.out.printf(".");
-				} else if (event instanceof CommandEvent) {
-					System.out.printf(".");
-				} else if (event instanceof ErrorEvent) {
-					System.out.printf(".");
-				} else if (event instanceof InfoEvent) {
-					System.out.printf(".");
-				} else if (event instanceof NavigationEvent) {
-					System.out.printf(".");
-				} else if (event instanceof SystemEvent) {
-					System.out.printf(".");
-					// ---------
-				} else if (event instanceof CompletionEvent) {
-					System.out.printf(".");
-					// ---------
-				} else if (event instanceof TestRunEvent) {
-					System.out.printf(".");
-					// ---------
-				} else if (event instanceof UserProfileEvent) {
-					System.out.printf(".");
-					// ---------
-				} else if (event instanceof VersionControlEvent) {
-					System.out.printf(".");
-					// ---------
-				} else if (event instanceof BuildEvent) {
-					System.out.printf(".");
-				} else if (event instanceof DebuggerEvent) {
-					System.out.printf(".");
-				} else if (event instanceof DocumentEvent) {
-					System.out.printf(".");
-				} else if (event instanceof EditEvent) {
-					System.out.printf(".");
-				} else if (event instanceof FindEvent) {
-					System.out.printf(".");
-				} else if (event instanceof IDEStateEvent) {
-					System.out.printf(".");
-				} else if (event instanceof InstallEvent) {
-					System.out.printf(".");
-				} else if (event instanceof SolutionEvent) {
-					System.out.printf(".");
-				} else if (event instanceof UpdateEvent) {
-					System.out.printf(".");
-				} else if (event instanceof WindowEvent) {
-					System.out.printf(".");
-				} else {
-					throw new RuntimeException("??");
-				}
-			}
-			System.out.println();
-		}
-
-		System.out.println(new Date());
-	}
-
 	/**
-	 * finds all users in a given folder. Remember that events are grouped by
-	 * user, so each .zip file corresponds to one user.
+	 * 1: Find all users in the dataset.
 	 */
-	public static List<String> findAllUsers(String dir) {
+	public static List<String> findAllUsers() {
+		// This step is straight forward, as events are grouped by user. Each
+		// .zip file in the dataset corresponds to one user.
+
 		List<String> zips = Lists.newLinkedList();
-		for (File f : FileUtils.listFiles(new File(dir), new String[] { "zip" }, true)) {
+		for (File f : FileUtils.listFiles(new File(DIR_USERDATA), new String[] { "zip" }, true)) {
 			zips.add(f.getAbsolutePath());
 		}
 		return zips;
 	}
 
 	/**
-	 * reads all captured events for a given user (= .zip file)
+	 * 2: Reading events
 	 */
-	public static List<String> read(String zipFile) {
-		List<String> res = Lists.newLinkedList();
-		try {
-			ReadingArchive ra = new ReadingArchive(new File(zipFile));
+	public static void readAllEvents() {
+		// each .zip file corresponds to a user
+		List<String> userZips = findAllUsers();
+
+		for (String user : userZips) {
+			// you can use our helper to open a file...
+			ReadingArchive ra = new ReadingArchive(new File(user));
+			// ...iterate over it...
 			while (ra.hasNext()) {
-				res.add(ra.getNextPlain());
+				// ... and desrialize the IDE event.
+				IIDEEvent e = ra.getNext(IIDEEvent.class);
+				// afterwards, you can process it as a Java object
+				process(e);
 			}
 			ra.close();
-		} catch (Exception e) {
-			e.printStackTrace();
 		}
-		return res;
+	}
+
+	/**
+	 * 3: Reading the plain JSON representation
+	 */
+	public static void readPlainEvents() {
+		// the example is basically the same as before, but...
+		List<String> userZips = findAllUsers();
+
+		for (String user : userZips) {
+			ReadingArchive ra = new ReadingArchive(new File(user));
+			while (ra.hasNext()) {
+				// ... sometimes it is easier to just read the JSON...
+				String json = ra.getNextPlain();
+				// .. and call the deserializer yourself.
+				IIDEEvent e = JsonUtils.fromJson(json, IIDEEvent.class);
+				process(e);
+
+				// Not all event bindings are very stable already, reading the
+				// JSON helps debugging possible bugs in the bindings
+			}
+			ra.close();
+		}
+	}
+
+	/**
+	 * 4: Processing events
+	 */
+	private static void process(IIDEEvent event) {
+		// once you have access to the instantiated event you can dispatch the
+		// type. As the events are not nested, we did not implement the visitor
+		// pattern, but resorted to instanceof checks.
+		if (event instanceof CommandEvent) {
+			// if the correct type is identified, you can cast it...
+			CommandEvent ce = (CommandEvent) event;
+			// ...and access the special context for this kind of event
+			System.out.println(ce.CommandId);
+		} else {
+			// there a many different event types to process, it is recommended
+			// that you browse the package to see all types and consult the
+			// website for the documentation of the semantics of each event...
+		}
 	}
 }
