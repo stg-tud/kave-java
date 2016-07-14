4a6d358877089dfe77fc60dc4fa24f6a957ef5d8
Ervina Cergani <cergani@st.informatik.tu-darmstadt.de>
2016-07-14 19:30:42 +0200

    minor change

--- cc.kave.episodes/src/main/java/cc/kave/episodes/export/PatternsOutput.java
+++ cc.kave.episodes/src/main/java/cc/kave/episodes/export/PatternsOutput.java
@@ -66,6 +66,7 @@ public class PatternsOutput {
 	public void write(int numbRepos, int freqThresh, double bidirectThresh) throws Exception {
 		Map<Integer, Set<Episode>> patterns = episodesProcessor.postprocess(numbRepos, freqThresh, bidirectThresh);
 		List<Event> events = mappingParser.parse(numbRepos);
+//		Logger.log("Number of unique event is %d", events.size());
 		int graphNumber = 0;
 
 		for (Map.Entry<Integer, Set<Episode>> entry : patterns.entrySet()) {
