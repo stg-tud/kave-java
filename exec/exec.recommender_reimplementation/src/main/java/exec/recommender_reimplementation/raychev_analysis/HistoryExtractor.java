9b3dff7d512a1f7d52b1f884d30569d9fad75da5
markuszm <markus_zimmermann-ffm@t-online.de>
2016-09-07 01:17:50 +0200

    Added statement limit for contexts to Raychev History Extraction

--- exec/exec.recommender_reimplementation/src/main/java/exec/recommender_reimplementation/raychev_analysis/HistoryExtractor.java
+++ exec/exec.recommender_reimplementation/src/main/java/exec/recommender_reimplementation/raychev_analysis/HistoryExtractor.java
@@ -32,13 +32,23 @@ import cc.kave.commons.pointsto.analysis.FieldSensitivity;
 import cc.kave.commons.pointsto.analysis.PointsToAnalysis;
 import cc.kave.commons.pointsto.analysis.PointsToContext;
 import cc.kave.commons.pointsto.analysis.unification.UnificationAnalysis;
+import cc.kave.commons.pointsto.evaluation.StatementCounterVisitor;
 import exec.recommender_reimplementation.java_transformation.JavaTransformationVisitor;
 
 public class HistoryExtractor {
 
-	public Set<ConcreteHistory> extractHistories(Context context) {
+	public final int STATEMENT_LIMIT = 500;
 
+	public static int filteredCount = 0;
+
+	public Set<ConcreteHistory> extractHistories(Context context) {
 		ISST sst = context.getSST();
+		Integer statementCount = sst.accept(new StatementCounterVisitor(), null);
+		if (statementCount > STATEMENT_LIMIT) {
+			filteredCount++;
+			return new HashSet<>();
+		}
+
 		JavaTransformationVisitor transformationVisitor = new JavaTransformationVisitor(sst);
 		sst = transformationVisitor.transform(sst, ISST.class);
 
