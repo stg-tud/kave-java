b9ffe3193559bd16693e5a99a93e1e27dd9c0b2c
ervinacergani <ervinacergani@s3934.dyn.hrz.tu-darmstadt.de>
2016-10-19 19:52:00 +0200

    some refactorings at PreprocessingFolded class

--- cc.kave.episodes/src/main/java/cc/kave/episodes/preprocessing/PreprocessingFolded.java
+++ cc.kave.episodes/src/main/java/cc/kave/episodes/preprocessing/PreprocessingFolded.java
@@ -1,12 +1,10 @@
 package cc.kave.episodes.preprocessing;
 
-import static cc.recommenders.assertions.Asserts.assertTrue;
-
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
-import cc.kave.episodes.export.EventStreamGenerator;
-import cc.kave.episodes.model.EventStream;
+import cc.kave.episodes.io.IndivReposParser;
 import cc.kave.episodes.model.events.Event;
 import cc.recommenders.io.Directory;
 import cc.recommenders.io.Logger;
@@ -17,81 +15,43 @@ import com.google.inject.name.Named;
 
 public class PreprocessingFolded {
 
-//	private Directory reposDir;
-//
-//	private ReposFoldedParser reposParser;
-//
-//	private static final int NUM_FOLDS = 10;
-//
-//	@Inject
-//	public PreprocessingFolded(@Named("repositories") Directory directory,
-//			ReposFoldedParser repositories) {
-//		this.reposDir = directory;
-//		this.reposParser = repositories;
-//	}
-//
-//	public void runPreparation() throws IOException {
-//
-//		List<List<EventStreamGenerator>> repositories = reposParser
-//				.generateFoldedEvents(NUM_FOLDS);
-//
-//		assertTrue(repositories.size() == NUM_FOLDS,
-//				"The repositories are not divided into n-folds!");
-//
-//		for (int curFold = 0; curFold < NUM_FOLDS; curFold++) {
-//			Logger.log("Generating foldNum %d/%d", (curFold + 1), NUM_FOLDS);
-//
-//			List<EventStream> trainingData = createTrainingData(curFold,
-//					NUM_FOLDS, repositories);
-//		}
-//	}
-//
-//	private List<EventStream> createTrainingData(int curFold, int numFolds,
-//			List<List<EventStreamGenerator>> repositories) {
-//
-//		List<List<EventStreamGenerator>> outs = Lists.newLinkedList();
-//
-//		int i = 0;
-//		for (List<EventStreamGenerator> list : repositories) {
-//			if (i != curFold) {
-//				outs.add(list);
-//			}
-//			i = (i + 1) % numFolds;
-//		}
-//		List<EventStream> trainingRepos = preprocescTraining(outs);
-//		return trainingRepos;
-//	}
-//
-//	private List<EventStream> preprocescTraining(
-//			List<List<EventStreamGenerator>> outs) {
-//		assertTrue(outs.size() == (NUM_FOLDS - 1),
-//				"Training data does not contain the right number of folds!");
-//		
-//		int numRepos = getNumRepos(outs);
-//		int partition = numRepos / 2;
-//		int curRepo = 0;
-//		List<EventStream> partitions = Lists.newLinkedList();
-//		List<Event> events = Lists.newLinkedList();
-//		EventStream stream = new EventStream();
-//
-//		for (List<EventStreamGenerator> fold : outs) {
-//			for (EventStreamGenerator repo : fold) {
-//				if (curRepo < partition) {
-//					events.addAll(repo.getEventStream());
-//				}
-//			}
-//
-//		}
-//
-//		return partitions;
-//	}
-//
-//	private int getNumRepos(List<List<EventStreamGenerator>> outs) {
-//		int numRepos = 0;
-//
-//		for (List<EventStreamGenerator> fold : outs) {
-//			numRepos += numRepos + fold.size();
-//		}
-//		return numRepos;
-//	}
+	private Directory reposDir;
+
+	private IndivReposParser reposParser;
+
+	private static final int NUM_FOLDS = 10;
+
+	@Inject
+	public PreprocessingFolded(@Named("repositories") Directory directory,
+			IndivReposParser repositories) {
+		this.reposDir = directory;
+		this.reposParser = repositories;
+	}
+
+	public void runPreparation() throws IOException {
+
+		Map<String, List<Event>> repos = reposParser.generateReposEvents();
+
+		for (int curFold = 0; curFold < NUM_FOLDS; curFold++) {
+			Logger.log("Generating foldNum %d/%d", (curFold + 1), NUM_FOLDS);
+
+			List<Event> trainingData = createTrainingData(curFold, NUM_FOLDS,
+					repos);
+		}
+	}
+
+	private List<Event> createTrainingData(int curFold, int numFolds,
+			Map<String, List<Event>> in) {
+
+		List<Event> outs = Lists.newLinkedList();
+
+		int i = 0;
+		for (Map.Entry<String, List<Event>> entry : in.entrySet()) {
+			if (i != curFold) {
+				outs.addAll(entry.getValue());
+			}
+			i = (i + 1) % numFolds;
+		}
+		return outs;
+	}
 }
