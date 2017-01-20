b14d29a54756dc08c78916d22a8d2637a53b9a14
ervinacergani <ervinacergani@s3841.dyn.hrz.tu-darmstadt.de>
2017-01-20 09:50:32 +0100

    fixed tests in PatternMapper

--- cc.kave.episodes/src/test/java/cc/kave/episodes/postprocessor/PatternMapperTest.java
+++ cc.kave.episodes/src/test/java/cc/kave/episodes/postprocessor/PatternMapperTest.java
@@ -1,6 +1,8 @@
 package cc.kave.episodes.postprocessor;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyDouble;
 import static org.mockito.Matchers.anyInt;
@@ -54,12 +56,18 @@ public class PatternMapperTest {
 		
 		catEpisode = Sets.newLinkedHashSet();
 		catEpisode.add(createEpisode(4, 1.0, "1", "2", "1>2"));
-		catEpisode.add(createEpisode(5, 0.8, "1", "3"));
+		catEpisode.add(createEpisode(5, 0.8, "1", "3", "3>1"));
+		catEpisode.add(createEpisode(4, 1.0, "1", "4", "1>4"));
 		episodes.put(2, catEpisode);
 		
 		catEpisode = Sets.newLinkedHashSet();
 		catEpisode.add(createEpisode(3, 0.5, "1", "2", "4", "1>2", "1>4"));
+		catEpisode.add(createEpisode(3, 0.7, "1", "2", "3", "1>2", "1>3"));
 		episodes.put(3, catEpisode);
+		
+		catEpisode = Sets.newLinkedHashSet();
+		catEpisode.add(createEpisode(2, 0.4, "1", "2", "3", "4", "1>2", "1>3", "1>4"));
+		episodes.put(4, catEpisode);
 		
 		sut = new PatternsMapper(episodeParser, episodeFilter, maxEpisodes);
 		
@@ -70,9 +78,8 @@ public class PatternMapperTest {
 	@Test
 	public void withMaximals() {
 		List<Episode> expected = Lists.newLinkedList();
-		expected.add(createEpisode(6, 1.0, "4"));
-		expected.add(createEpisode(5, 0.8, "1", "3"));
-		expected.add(createEpisode(3, 0.5, "1", "2", "4", "1>2", "1>4"));
+		expected.add(createEpisode(5, 0.8, "1", "3", "3>1"));
+		expected.add(createEpisode(2, 0.4, "1", "2", "3", "4", "1>2", "1>3", "1>4"));
 		
 		List<Episode> actuals = sut.getMapper(FREQUENCY, EpisodeType.GENERAL, true);
 		
@@ -87,14 +94,30 @@ public class PatternMapperTest {
 		expected.add(createEpisode(8, 1.0, "3"));
 		expected.add(createEpisode(6, 1.0, "4"));
 		expected.add(createEpisode(4, 1.0, "1", "2", "1>2"));
-		expected.add(createEpisode(5, 0.8, "1", "3"));
+		expected.add(createEpisode(5, 0.8, "1", "3", "3>1"));
+		expected.add(createEpisode(4, 1.0, "1", "4", "1>4"));
 		expected.add(createEpisode(3, 0.5, "1", "2", "4", "1>2", "1>4"));
+		expected.add(createEpisode(3, 0.7, "1", "2", "3", "1>2", "1>3"));
+		expected.add(createEpisode(2, 0.4, "1", "2", "3", "4", "1>2", "1>3", "1>4"));
 		
 		List<Episode> actuals = sut.getMapper(FREQUENCY, EpisodeType.GENERAL, false);
 		
-		assertEquals(episodes, actuals);
+		assertLists(expected, actuals);
 	}
 	
+
+	private void assertLists(List<Episode> expected, List<Episode> actuals) {
+		if (expected.size() != actuals.size()) {
+			fail();
+		}
+		for (Episode episode : expected) {
+			if (!actuals.contains(episode)) {
+				fail();
+			} 
+		}
+		assertTrue(true);
+	}
+
 	private Episode createEpisode(int freq, double entropy, String...strings) {
 		Episode episode = new Episode();
 		episode.setFrequency(freq);
