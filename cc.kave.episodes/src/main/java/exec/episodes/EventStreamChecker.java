b50812ec93c33f65f2e596d5ec7315b9403a6363
ervina <ervinacergani@s3159.dyn.hrz.tu-darmstadt.de>
2017-09-29 18:31:32 +0200

    small changes

--- cc.kave.episodes/src/main/java/exec/episodes/EventStreamChecker.java
+++ cc.kave.episodes/src/main/java/exec/episodes/EventStreamChecker.java
@@ -4,14 +4,18 @@ import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import javax.inject.Inject;
 
+import cc.kave.commons.model.naming.codeelements.IMethodName;
 import cc.kave.episodes.io.EventStreamIo;
 import cc.kave.episodes.model.events.Fact;
+import cc.recommenders.datastructures.Tuple;
 import cc.recommenders.io.Logger;
 
 import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 
 public class EventStreamChecker {
 
@@ -22,30 +26,42 @@ public class EventStreamChecker {
 		this.streamIo = streamIo;
 	}
 
-	private Map<List<Fact>, Integer> methodOccs = Maps.newHashMap();
+	private Map<List<Fact>, Set<IMethodName>> methodOccs = Maps.newHashMap();
 
 	public void duplicates(int frequency) throws IOException {
-		List<List<Fact>> stream = streamIo.parseStream(frequency);
+		List<Tuple<IMethodName, List<Fact>>> stream = streamIo
+				.readStreamObject(frequency);
 
-		for (List<Fact> method : stream) {
+		for (Tuple<IMethodName, List<Fact>> tuple : stream) {
 			boolean isContained = false;
 
-			for (Map.Entry<List<Fact>, Integer> entry : methodOccs.entrySet()) {
-				isContained = assertLists(method, entry.getKey());
+			for (Map.Entry<List<Fact>, Set<IMethodName>> entry : methodOccs
+					.entrySet()) {
+				isContained = assertLists(tuple.getSecond(), entry.getKey());
 
 				if (isContained) {
-					int counter = methodOccs.get(entry.getKey());
-					methodOccs.put(entry.getKey(), counter + 1);
-					
+					methodOccs.get(entry.getKey()).add(tuple.getFirst());
+
 					Logger.log("Cur: %s", entry.getKey().toString());
-					Logger.log("New: %s", method.toString());
+					Logger.log("New: %s", tuple.getSecond().toString());
 					Logger.log("");
 					break;
 				}
 			}
 			if (!isContained) {
-				methodOccs.put(method, 1);
+				methodOccs.put(tuple.getSecond(),
+						Sets.newHashSet(tuple.getFirst()));
 			}
+		}
+		Logger.log("Ouptutting duplicate code occurrences ...");
+		for (Map.Entry<List<Fact>, Set<IMethodName>> entry : methodOccs
+				.entrySet()) {
+			Logger.log("Method: %s", entry.getKey().toString());
+			
+			for (IMethodName methodNames : entry.getValue()) {
+				Logger.log("%s", methodNames.getIdentifier());
+			}
+			Logger.log("");
 		}
 	}
 
