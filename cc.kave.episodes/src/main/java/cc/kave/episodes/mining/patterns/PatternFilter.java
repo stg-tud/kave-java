0033742834d3dad4c44b2df9ed45f0aea4ec507d
ervina <ervinacergani@pc-cergani.st.informatik.tu-darmstadt.de>
2018-08-28 14:39:32 +0200

    added tests for PatternFilter

--- cc.kave.episodes/src/main/java/cc/kave/episodes/mining/patterns/PatternFilter.java
+++ cc.kave.episodes/src/main/java/cc/kave/episodes/mining/patterns/PatternFilter.java
@@ -2,16 +2,16 @@ package cc.kave.episodes.mining.patterns;
 
 import static cc.recommenders.assertions.Asserts.assertTrue;
 
-import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
 
 import javax.inject.Inject;
 
-import com.google.common.collect.Sets;
-
 import cc.kave.episodes.model.Episode;
 import cc.kave.episodes.model.EpisodeType;
+
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 
 public class PatternFilter {
 
@@ -42,42 +42,44 @@ public class PatternFilter {
 		}
 	}
 
-	public Set<Episode> subPatterns(EpisodeType type,
-			Map<Integer, Set<Episode>> episodes, int thf, double the,
-			double thsp) throws Exception {
-		Map<Integer, Set<Episode>> patterns = filter(type, episodes, thf, the);
+	public Map<Integer, Set<Episode>> subEpisodes(
+			Map<Integer, Set<Episode>> episodes, double thsp) throws Exception {
 
-		assertTrue(!patterns.isEmpty(),
-				"The list of learned patterns is empty!");
+		assertTrue(!episodes.isEmpty(), "The list of episodes is empty!");
 
-		Set<Episode> results = Sets.newLinkedHashSet();
-		for (Map.Entry<Integer, Set<Episode>> entry : patterns.entrySet()) {
-			if (entry.getKey() == (patterns.size() + 1)) {
-				results.addAll(entry.getValue());
+		Map<Integer, Set<Episode>> results = Maps.newLinkedHashMap();
+		for (Map.Entry<Integer, Set<Episode>> entry : episodes.entrySet()) {
+			if (entry.getKey() == 1) {
+				continue;
+			}
+			if (entry.getKey() == episodes.size()) {
+				results.put(entry.getKey(), entry.getValue());
 				break;
 			}
-			results.addAll(getPatterns(entry.getValue(),
-					patterns.get(entry.getKey() + 1), thsp));
+			Set<Episode> superepisodes = episodes.get(entry.getKey() + 1);
+			results.put(entry.getKey(),
+					getSubEpisodes(entry.getValue(), superepisodes, thsp));
 		}
 		return results;
 	}
 
-	private Set<Episode> getPatterns(Set<Episode> subPatterns,
-			Set<Episode> patterns, double thsp) {
+	private Set<Episode> getSubEpisodes(Set<Episode> subEpisodes,
+			Set<Episode> superEpisodes, double thsp) {
 		Set<Episode> results = Sets.newLinkedHashSet();
 
-		for (Episode sp : subPatterns) {
-			if (!isSubpattern(sp, patterns, thsp)) {
+		for (Episode sp : subEpisodes) {
+			if (!isSubepisode(sp, superEpisodes, thsp)) {
 				results.add(sp);
 			}
 		}
 		return results;
 	}
 
-	private boolean isSubpattern(Episode sp, Set<Episode> patterns, double thsp) {
-		for (Episode p : patterns) {
-			if (p.getFacts().containsAll(sp.getFacts())
-					&& ((p.getFrequency() / sp.getFrequency()) < thsp)) {
+	private boolean isSubepisode(Episode subepisode, Set<Episode> superepisode,
+			double thsp) {
+		for (Episode ep : superepisode) {
+			if (ep.getFacts().containsAll(subepisode.getFacts())
+					&& ((ep.getFrequency() / subepisode.getFrequency()) < thsp)) {
 				return true;
 			}
 		}
--- cc.kave.episodes/src/test/java/cc/kave/episodes/mining/patterns/PatternFilterTest.java
+++ cc.kave.episodes/src/test/java/cc/kave/episodes/mining/patterns/PatternFilterTest.java
@@ -6,6 +6,8 @@ import static org.mockito.Matchers.anyMap;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.Map;
 import java.util.Set;
 
@@ -18,8 +20,10 @@ import org.mockito.MockitoAnnotations;
 
 import cc.kave.episodes.model.Episode;
 import cc.kave.episodes.model.EpisodeType;
+import cc.recommenders.exceptions.AssertionException;
 
 import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 
 public class PatternFilterTest {
 
@@ -33,7 +37,6 @@ public class PatternFilterTest {
 	@Mock
 	private ParallelPatterns parallels;
 
-	private static final int FREQUENCY = 5;
 	private static final int FREQTHRESH = 5;
 	private static final double ENTROPY = 0.5;
 	private static final double THRESHSUB = 1.0;
@@ -85,5 +88,57 @@ public class PatternFilterTest {
 		sut.filter(EpisodeType.PARALLEL, episodes, FREQTHRESH, ENTROPY);
 
 		verify(parallels).filter(anyMap(), anyInt());
+	}
+	
+	@Test
+	public void emptyEpisodes() throws Exception {
+		thrown.expect(AssertionException.class);
+		thrown.expectMessage("The list of episodes is empty!");
+		
+		sut.subEpisodes(episodes, THRESHSUB);
+	}
+	
+	@Test
+	public void subepisodes() throws Exception {
+		episodes.put(1, Sets.newHashSet(createEpisode(2, 1.0, "a")));
+		
+		Set<Episode> epSet = Sets.newLinkedHashSet();
+		epSet.add(createEpisode(5, 1.0, "a", "b", "a>b"));
+		epSet.add(createEpisode(6,  0.5, "c", "d"));
+		epSet.add(createEpisode(5, 1.0, "e", "f", "e>f"));
+		episodes.put(2, epSet);
+		
+		epSet = Sets.newLinkedHashSet();
+		epSet.add(createEpisode(4, 0.5, "a", "b", "c", "a>b", "a>c"));
+		epSet.add(createEpisode(6, 0.5, "c", "d", "e", "c>d", "c>e"));
+		epSet.add(createEpisode(2, 0.8, "b", "d", "f"));
+		episodes.put(3, epSet);
+		
+		Map<Integer, Set<Episode>> expected = Maps.newLinkedHashMap();
+		epSet = Sets.newLinkedHashSet();
+		epSet.add(createEpisode(6,  0.5, "c", "d"));
+		epSet.add(createEpisode(5, 1.0, "e", "f", "e>f"));
+		expected.put(2, epSet);
+		
+		epSet = Sets.newLinkedHashSet();
+		epSet.add(createEpisode(4, 0.5, "a", "b", "c", "a>b", "a>c"));
+		epSet.add(createEpisode(6, 0.5, "c", "d", "e", "c>d", "c>e"));
+		epSet.add(createEpisode(2, 0.8, "b", "d", "f"));
+		expected.put(3, epSet);
+		
+		Map<Integer, Set<Episode>> actuals = sut.subEpisodes(episodes, THRESHSUB);
+		
+		assertEquals(expected, actuals);
+	}
+
+	private Episode createEpisode(int frequency, double entropy, String...strings) {
+		Episode episode = new Episode();
+		episode.setFrequency(frequency);
+		episode.setEntropy(entropy);
+		
+		for (String string :  strings) {
+			episode.addFact(string);
+		}
+		return episode;
 	}
 }
