4e0187e23c9db613b48410912acf5c7a47fad003
ervina <ervinacergani@172.16.88.41>
2018-10-18 17:58:00 +0200

    improved running time in PatternFilter

--- cc.kave.episodes/src/main/java/cc/kave/episodes/mining/patterns/PatternFilter.java
+++ cc.kave.episodes/src/main/java/cc/kave/episodes/mining/patterns/PatternFilter.java
@@ -9,6 +9,7 @@ import javax.inject.Inject;
 
 import cc.kave.episodes.model.Episode;
 import cc.kave.episodes.model.EpisodeType;
+import cc.kave.episodes.model.events.Fact;
 
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
@@ -48,28 +49,64 @@ public class PatternFilter {
 		assertTrue(!episodes.isEmpty(), "The list of episodes is empty!");
 
 		Map<Integer, Set<Episode>> results = Maps.newLinkedHashMap();
-		for (Map.Entry<Integer, Set<Episode>> entry : episodes.entrySet()) {
+
+		Map<Integer, Map<Set<Fact>, Set<Episode>>> episodeStruct = getEpStruct(episodes);
+
+		for (Map.Entry<Integer, Map<Set<Fact>, Set<Episode>>> entry : episodeStruct
+				.entrySet()) {
 			if (entry.getKey() == 1) {
 				continue;
 			}
 			if (entry.getKey() == episodes.size()) {
-				results.put(entry.getKey(), entry.getValue());
+				results.put(entry.getKey(), episodes.get(entry.getKey()));
 				break;
 			}
-			Set<Episode> superepisodes = episodes.get(entry.getKey() + 1);
-			results.put(entry.getKey(),
-					getSubEpisodes(entry.getValue(), superepisodes, thsp));
+			results.put(
+					entry.getKey(),
+					getSuperEpisodes(entry.getValue(),
+							episodeStruct.get(entry.getKey() + 1), thsp));
 		}
 		return results;
 	}
 
-	private Set<Episode> getSubEpisodes(Set<Episode> subEpisodes,
-			Set<Episode> superEpisodes, double thsp) {
-		Set<Episode> results = Sets.newLinkedHashSet();
+	private Map<Integer, Map<Set<Fact>, Set<Episode>>> getEpStruct(
+			Map<Integer, Set<Episode>> episodes) {
+		Map<Integer, Map<Set<Fact>, Set<Episode>>> results = Maps
+				.newLinkedHashMap();
+		for (Map.Entry<Integer, Set<Episode>> entry : episodes.entrySet()) {
+			results.put(entry.getKey(), getStruct(entry.getValue()));
+		}
+		return results;
+	}
 
-		for (Episode sp : subEpisodes) {
-			if (!isSubepisode(sp, superEpisodes, thsp)) {
-				results.add(sp);
+	private Map<Set<Fact>, Set<Episode>> getStruct(Set<Episode> episodes) {
+		Map<Set<Fact>, Set<Episode>> results = Maps.newLinkedHashMap();
+		for (Episode episode : episodes) {
+			Set<Fact> events = episode.getEvents();
+			if (results.containsKey(events)) {
+				results.get(events).add(episode);
+			} else {
+				results.put(events, Sets.newHashSet(episode));
+			}
+		}
+		return results;
+	}
+
+	private Set<Episode> getSuperEpisodes(Map<Set<Fact>, Set<Episode>> subs,
+			Map<Set<Fact>, Set<Episode>> supers, double thsp) {
+		Set<Episode> results = Sets.newLinkedHashSet();
+		for (Map.Entry<Set<Fact>, Set<Episode>> smallEntry : subs.entrySet()) {
+			Set<Episode> potSSupers = Sets.newLinkedHashSet();
+			for (Map.Entry<Set<Fact>, Set<Episode>> bigEntry : supers
+					.entrySet()) {
+				if (bigEntry.getKey().containsAll(smallEntry.getKey())) {
+					potSSupers.addAll(bigEntry.getValue());
+				}
+			}
+			for (Episode episode : smallEntry.getValue()) {
+				if (!isSubepisode(episode, potSSupers, thsp)) {
+					results.add(episode);
+				}
 			}
 		}
 		return results;
